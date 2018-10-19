06643d07b7a50014fa30452fc6f9aac6e8f054bf
Emory Merryman <emory.merryman@gmail.com>
2018-10-19 17:38:53 -0400

    this is a kludgy solution

--- custom/bash/default.nix
+++ custom/bash/default.nix
@@ -7,13 +7,12 @@ stdenv.mkDerivation rec {
   installPhase = ''
     mkdir $out &&
       mkdir $out/etc &&
-      nix-build bash.nix &&
-      cp result $out/etc &&
+      cp bash.nix $out/etc &&
       mkdir $out/scripts &&
       cp bash.sh $out/scripts &&
       chmod 0500 $out/scripts/bash.sh &&
       mkdir $out/bin &&
-      makeWrapper $out/scripts/bash.sh $out/bin/my-bash --set PATH ${lib.makeBinPath [ docker ]} --set STORE_DIR $out &&
+      makeWrapper $out/scripts/bash.sh $out/bin/my-bash --set PATH ${lib.makeBinPath [ docker mktemp coreutils ]} --set STORE_DIR $out &&
       true
   '';
 }
--- custom/bash/src/bash.sh
+++ custom/bash/src/bash.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-docker container run --interactive --tty --rm bash
+cd $(mktemp -d) &&
+    cp ${STORE_DIR}/etc/bash.nix . &&
+    /run/current-system/sw/bin/nix-build bash.nix &&
+    docker image load < result &&
+    docker container run --interactive --tty --rm bash
