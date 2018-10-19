0d77fbaa2f61b23f850942e82ce4d0b9bfa549ae
Emory Merryman <emory.merryman@gmail.com>
2018-10-19 16:00:24 -0400

    use gui

--- configuration.nix
+++ configuration.nix
@@ -1,15 +1,41 @@
 { config, pkgs, ... }:
 {
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  hardware.pulseaudio.enable = true;
+  i18n = {
+    consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "us";
+    defaultLocale = "en_US.UTF-8";
+  };
   imports = [
     ./hardware-configuration.nix
     ./installed/password.nix
   ];
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  networking = {
+    networkmanager = {
+      enable = true;
+      unmanaged = [ "interface-name:ve-*" ];
+    };
+    nat = {
+      enable = true;
+      internalInterfaces = [ "ve-+" ];
+      externalInterface = "wl01";
+    };
+  };
+  services.xserver = {
+    enable = true;
+    windowManager.i3.enable = true;
+    libinput.enable = true;
+  };
+  sound.enable = true;
+  time.timeZone = "US/Eastern";
+  virtualisation.docker.enable = true;
+  virtualisation.virtualbox.host.enable = true;
   users.mutableUsers = false;
   users.extraUsers.user.isNormalUser = true;
   users.extraUsers.user.uid = 1000;
-  users.extraUsers.user.extraGroups = [ "wheel" ];
+  users.extraUsers.user.extraGroups = [ "wheel" "docker" ];
   users.extraUsers.user.packages = [
     (import ./installed/init-read-only-pass/default.nix { inherit pkgs; })
     (import ./installed/init-wifi/default.nix { inherit pkgs; })
