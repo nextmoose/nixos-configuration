1f021fe330c10968d2e22a022514136980b585e6
Emory Merryman <emory.merryman@gmail.com>
2018-10-19 12:29:08 -0400

    fix error

--- configuration.nix
+++ configuration.nix
@@ -13,7 +13,7 @@
   users.extraUsers.user.packages = [
     (import ./installed/init-read-only-pass/default.nix { inherit pkgs; })
     (import ./installed/init-wifi/default.nix { inherit pkgs; })
-    pass
+    pkgs.pass
   ];
   system.stateVersion = "18.03";
 }
