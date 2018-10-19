bce775c9e55597dcb7401d0c00038a21512428f1
Emory Merryman <emory.merryman@gmail.com>
2018-10-19 15:23:44 -0400

    do it

--- custom/init-user-experience/default.nix
+++ custom/init-user-experience/default.nix
@@ -12,7 +12,7 @@ stdenv.mkDerivation rec {
       cp init-user-experience.sh $out/scripts &&
       chmod 0500 $out/scripts/init-user-experience.sh &&
       mkdir $out/bin &&
-      makeWrapper $out/scripts/init-user-experience.sh $out/bin/init-user-experience --set PATH ${lib.makeBinPath [  ]} --set STORE_DIR $out &&
+      makeWrapper $out/scripts/init-user-experience.sh $out/bin/init-user-experience --set PATH ${lib.makeBinPath [ wifi initreadonlypass coreutils pass git  ]} --set STORE_DIR $out &&
       true
   '';
 }
