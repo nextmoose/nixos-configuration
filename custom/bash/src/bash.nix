5e1a3df8fb1f92c9d2bd70cc21ceba6e00cd6ffc
Emory Merryman <emory.merryman@gmail.com>
2018-10-19 17:16:10 -0400

    wtf

--- custom/bash/src/bash.nix
+++ custom/bash/src/bash.nix
@@ -1,17 +1,25 @@
 { pkgs ? import <nixpkgs> {} }:
 with pkgs;
+let
+  entrypoint = writeScript "entrypoint.sh" ''
+    #!${stdenv.shell}
+
+    set -e
+    echo hello
+  '';
+in
 dockerTools.buildImage {
   name = "bash";
   runAsRoot = ''
+    #!${stdenv.shell}
     ${dockerTools.shadowSetup}
     mkdir /home &&
       useradd --create-home user &&
       true
   '';
-  contents = [ pkgs.bash ];
+  contents = [ pkgs.coreutils ];
   config = {
     Cmd = [ ];
-    Entrypoint = [ pkgs.bash ];
-    User = "user";
+    Entrypoint = [ entrypoint ];
   };
 }
