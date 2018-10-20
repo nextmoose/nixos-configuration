991403763f486008364ed6ab544502cca225f596
Emory Merryman <emory.merryman@gmail.com>
2018-10-20 09:07:54 -0400

    rework

--- configuration.nix
+++ configuration.nix
@@ -52,24 +52,26 @@
     };
     virtualbox.host.enable = true;
   };
-  users.mutableUsers = false;
-  users.extraUsers.user.isNormalUser = true;
-  users.extraUsers.user.uid = 1000;
-  users.extraUsers.user.extraGroups = [ "wheel" "docker" ];
-  users.extraUsers.user.packages = [
-    (import ./installed/init-read-only-pass/default.nix { inherit pkgs; })
-    (import ./installed/init-wifi/default.nix { inherit pkgs; })
-    (import ./custom/init-user-experience/default.nix { inherit pkgs; })
-    (import ./custom/update-nixos/default.nix { inherit pkgs; })
-    (import ./custom/bash/default.nix { inherit pkgs; })
-    (import ./custom/chromium/default.nix { inherit pkgs; })
-    (import ./custom/firefox/default.nix { inherit pkgs; })
-    (import ./custom/personal/default.nix { inherit pkgs; })
-    pkgs.pass
-    pkgs.git
-    pkgs.emacs
-    pkgs.networkmanager
-    pkgs.gnome3.gnome-terminal
-  ];
+  users = {
+    mutableUsers = false;
+    extraUsers.user.isNormalUser = true;
+    extraUsers.user.uid = 1000;
+    extraUsers.user.extraGroups = [ "wheel" "docker" ];
+    extraUsers.user.packages = [
+      (import ./installed/init-read-only-pass/default.nix { inherit pkgs; })
+      (import ./installed/init-wifi/default.nix { inherit pkgs; })
+      (import ./custom/init-user-experience/default.nix { inherit pkgs; })
+      (import ./custom/update-nixos/default.nix { inherit pkgs; })
+      (import ./custom/bash/default.nix { inherit pkgs; })
+      (import ./custom/firefox/default.nix { inherit pkgs; })
+      (import ./custom/personal/default.nix { inherit pkgs; })
+      pkgs.pass
+      pkgs.git
+      pkgs.emacs
+      pkgs.networkmanager
+      pkgs.gnome3.gnome-terminal
+      pkgs.chromium
+    ];
+  };
   system.stateVersion = "18.03";
 }
