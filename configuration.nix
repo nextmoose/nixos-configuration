57ec7a60612c87e5deca5cad1d091131d94ad0ec
Emory Merryman <emory.merryman@gmail.com>
2018-10-21 06:58:05 -0400

    adding init-development-environment

--- configuration.nix
+++ configuration.nix
@@ -12,7 +12,27 @@
       config = { config, pkgs, ...}:
       {
         programs.bash.shellInit = ''
-	  ${init-development-environment}/bin/init-development-environment &&
+	  ${init-development-environment}/bin/init-development-environment \
+	    --upstream-host github.com \
+	    --upstream-user git \
+	    --upstream-port 22 \
+	    --upstream-organization rebelplutonium \
+	    --upstream-repository nixos-configuration \
+	    --upstream-branch master \
+	    --origin-host github.com \
+	    --origin-user git \
+	    --origin-port 22 \
+	    --origin-organization nextmoose \
+	    --origin-repository nixos-configuration \
+	    --origin-branch level-2 \
+	    --report-host github.com \
+	    --report-user git \
+	    --report-port 22 \
+	    --report-organization rebelplutonium \
+	    --report-repository nixos-configuration \
+	    --committer-name "Emory Merryman"
+	    --committer-email emory.merryman@gmail.com
+	    &&
 	    true
 	'';
 	services.mingetty.autologinUser = "user";
