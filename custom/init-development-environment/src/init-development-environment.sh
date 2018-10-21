9c31b0e80322a1bd87c66ee8507ec8f28cb3766e
Emory Merryman <emory.merryman@gmail.com>
2018-10-21 07:20:56 -0400

    eliminate bogus condition

--- custom/init-development-environment/src/init-development-environment.sh
+++ custom/init-development-environment/src/init-development-environment.sh
@@ -5,104 +5,129 @@ do
     case "${1}" in
 	--upstream-host)
 	    UPSTREAM_HOST="${2}" &&
-		shift 2
+		shift 2 &&
+		true
 	    ;;
 	--upstream-user)
 	    UPSTREAM_USER="${2}" &&
-		shift 2
+		shift 2 &&
+		true
 	    ;;
 	--upstream-port)
 	    UPSTREAM_PORT="${2}" &&
-		shift 2
+		shift 2 &&
+		true
 	    ;;
 	--upstream-organization)
 	    UPSTREAM_ORGANIZATION="${2}" &&
-		shift 2
+		shift 2 &&
+		true
 	    ;;
 	--upstream-repository)
 	    UPSTREAM_REPOSITORY="${2}" &&
-		shift 2
+		shift 2 &&
+		true
 	    ;;
 	--upstream-repository)
 	    UPSTREAM_REPOSITORY="${2}" &&
-		shift 2
+		shift 2 &&
+		true
 	    ;;
 	--upstream-branch)
 	    UPSTREAM_BRANCH="${2}" &&
-		shift 2
+		shift 2 &&
+		true
 	    ;;
 	--origin-host)
 	    ORIGIN_HOST="${2}" &&
-		shift 2
+		shift 2 &&
+		true
 	    ;;
 	--origin-user)
 	    ORIGIN_USER="${2}" &&
-		shift 2
+		shift 2 &&
+		true
 	    ;;
 	--origin-port)
 	    ORIGIN_PORT="${2}" &&
-		shift 2
+		shift 2 &&
+		true
 	    ;;
 	--origin-organization)
 	    ORIGIN_ORGANIZATION="${2}" &&
-		shift 2
+		shift 2 &&
+		true
 	    ;;
 	--origin-repository)
 	    ORIGIN_REPOSITORY="${2}" &&
-		shift 2
+		shift 2 &&
+		true
 	    ;;
 	--origin-repository)
 	    ORIGIN_REPOSITORY="${2}" &&
-		shift 2
+		shift 2 &&
+		true
 	    ;;
 	--origin-branch)
 	    ORIGIN_BRANCH="${2}" &&
-		shift 2
+		shift 2 &&
+		true
 	    ;;
 	--report-host)
 	    REPORT_HOST="${2}" &&
-		shift 2
+		shift 2 &&
+		true
 	    ;;
 	--report-user)
 	    REPORT_USER="${2}" &&
-		shift 2
+		shift 2 &&
+		true
 	    ;;
 	--report-port)
 	    REPORT_PORT="${2}" &&
-		shift 2
+		shift 2 &&
+		true
 	    ;;
 	--report-organization)
 	    REPORT_ORGANIZATION="${2}" &&
-		shift 2
+		shift 2 &&
+		true
 	    ;;
 	--report-repository)
 	    REPORT_REPOSITORY="${2}" &&
-		shift 2
+		shift 2 &&
+		true
 	    ;;
 	--report-repository)
 	    REPORT_REPOSITORY="${2}" &&
-		shift 2
+		shift 2 &&
+		true
 	    ;;
 	--report-branch)
 	    REPORT_BRANCH="${2}" &&
-		shift 2
+		shift 2 &&
+		true
 	    ;;
 	--committer-name)
 	    COMMITTER_NAME="${2}" &&
-		shift 2
+		shift 2 &&
+		true
 	    ;;
 	--committer-email)
 	    COMMITTER_EMAIL="${2}" &&
-		shift 2
+		shift 2 &&
+		true
 	    ;;
 	*)
 	    echo Unknown Option &&
 		echo ${1} &&
 		echo ${0} &&
 		echo ${@} &&
-		exit 64
+		exit 64 &&
+		true
 	    ;;
-    esac
+    esac &&
+	true
 done &&
     echo COMMITTER_NAME COMMITTER_EMAIL | while read VAR
     do
@@ -110,7 +135,8 @@ done &&
 	    if [ -z "${VAR}" ]
 	    then
 		echo Unspecified ${VAR} &&
-		    exit 66
+		    exit 66 &&
+		    true
 	    fi &&
 	    true
     done &&
