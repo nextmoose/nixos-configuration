95a9cf95e31b20461ccbeec5b35ad847fec13c54
Emory Merryman <emory.merryman@gmail.com>
2018-10-21 07:00:10 -0400

    reverting firefox

--- custom/firefox/default.nix
+++ custom/firefox/default.nix
@@ -1,15 +1,18 @@
 { pkgs ? import <nixpkgs> {} }:
 with import <nixpkgs> {};
 stdenv.mkDerivation rec {
-  name = "init-development-environment";
+  name = "firefox";
   src = ./src;
   buildInputs = [ makeWrapper ];
   installPhase = ''
     mkdir $out &&
+      mkdir $out/etc &&
+      cp firefox.nix $out/etc &&
+      mkdir $out/scripts &&
       cp firefox.sh $out/scripts &&
-      chmod 0500 $out/scripts/init-development-environment.sh &&
+      chmod 0500 $out/scripts/firefox.sh &&
       mkdir $out/bin &&
-      makeWrapper $out/scripts/init-development-environment.sh $out/bin/init-develpment-environment --set PATH ${lib.makeBinPath [ init-read-only-pass pass git ]} --set STORE_DIR $out &&
+      makeWrapper $out/scripts/firefox.sh $out/bin/firefox --set PATH ${lib.makeBinPath [ docker mktemp coreutils ]} --set STORE_DIR $out &&
       true
   '';
 }
