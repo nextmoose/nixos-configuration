f0a25737d7d415ef32d6bae61819160076a3b0a7
Emory Merryman <emory.merryman@gmail.com>
2018-10-22 12:11:21 -0400

    starting to clean up

--- custom/bash/src/bash.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd $(mktemp -d) &&
-    cp ${STORE_DIR}/etc/bash.nix . &&
-    /run/current-system/sw/bin/nix-build bash.nix &&
-    docker image load < result &&
-    docker container run --interactive --tty --rm bash
