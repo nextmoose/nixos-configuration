154eff62291411c56b7b37d7382990526b3bda14
Emory Merryman <emory.merryman@gmail.com>
2018-10-23 14:10:42 -0400

    add turnover

--- custom/expressions/pass-utils/src/turnover.sh
+++ custom/expressions/pass-utils/src/turnover.sh
@@ -15,7 +15,7 @@ TEMP_DIR=$(mktemp -d) &&
 	    LAST_COMMIT_DATE=$(pass git log -1 --format=%ct ${FILE}) &&
 	    AGE=$(($(date +%s)-${LAST_COMMIT_DATE})) &&
 	    SIZEAGE=$(echo ${AGE} | wc --bytes) &&
-	    echo ${FILE%.*} ${SIZESIZE} ${SIZEAGE} ${SIZE} ${AGE} >> ${TEMP_DIR}/results.txt $(date --date ${LAST_COMMIT_DATE} + %F) &&
+	    echo ${FILE%.*} ${SIZESIZE} ${SIZEAGE} ${SIZE} ${AGE} >> ${TEMP_DIR}/results.txt $(date --date @${LAST_COMMIT_DATE} + %F) &&
 	    rm --force ${TEMP_DIR}/output/$(basename ${FILE%.*}) &&
 	    true
     done > ${TEMP_DIR}/log2.txt 2>&1 &&
