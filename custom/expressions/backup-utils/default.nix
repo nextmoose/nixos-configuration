69cd2779df243c5f10d2d57756fb2e9a55eff705
Emory Merryman <emory.merryman@gmail.com>
2018-10-23 16:47:11 -0400

    add archive

--- custom/expressions/backup-utils/default.nix
+++ custom/expressions/backup-utils/default.nix
@@ -10,7 +10,7 @@ stdenv.mkDerivation rec {
       cp *.sh $out/scripts &&
       chmod 0500 $out/scripts/*.sh &&
       mkdir $out/bin &&
-      makeWrapper $out/scripts/archive.sh $out/bin/archive --set PATH ${lib.makeBinPath [ "/run/wrappers" coreutils utillinux lvm2 gnutar gzip cdrkit dvdisaster ]} &&
+      makeWrapper $out/scripts/archive.sh $out/bin/archive --set PATH ${lib.makeBinPath [ "/run/wrappers" coreutils utillinux lvm2 gnutar gzip cdrkit dvdisaster gnupg ]} &&
       makeWrapper $out/scripts/snapshot.sh $out/bin/snapshot --set PATH ${lib.makeBinPath [ "/run/wrappers" coreutils utillinux lvm2 rsync ]} &&
       true
   '';
