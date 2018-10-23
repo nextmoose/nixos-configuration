283c46060235da799c289ed18e2ce0895eb79673
Emory Merryman <emory.merryman@gmail.com>
2018-10-23 11:00:19 -0400

    add browser secrets

---
+++ custom/expressions/container-initializations/src/browser-secrets.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+secrets \
+    --canonical-host github.com \
+    --canonical-organization nextmoose \
+    --canonical-repository secrets \
+    --canonical-branch master \
+    &&
+    ssh-remote \
+	--remote upstream \
+	--host github.com \
+	--user git \
+	--port 22 \
+	&&
+    ssh-remote \
+	--remote origin \
+	--host github.com \
+	--user git \
+	--port 22 \
+	&&
+    ssh-remote \
+	--remote report \
+	--host github.com \
+	--user git \
+	--port 22 \
+	&&
+    pass \
+	--upstream-organization nextmoose \
+	--upstream-repository browser-secrets \
+      	--origin-organization nextmoose \
+	--origin-repository browser-secrets \
+	--report-organization rebelplutonium \
+	--report-repository browser-secrets \
+	&&
+    true
---
+++ custom/expressions/initialization-utils/src/pass.sh
@@ -0,0 +1,83 @@
+#!/bin/sh
+
+while [ "${#}" -gt 0 ]
+do
+    case "${1}" in
+	--upstream-organization)
+	    UPSTREAM_ORGANIZATION="${2}" &&
+		shift 2 &&
+		true
+	    ;;
+	--upstream-repository)
+	    UPSTREAM_REPOSITORY="${2}" &&
+		shift 2 &&
+		true
+	    ;;
+	--origin-organization)
+	    ORIGIN_ORGANIZATION="${2}" &&
+		shift 2 &&
+		true
+	    ;;
+	--origin-repository)
+	    ORIGIN_REPOSITORY="${2}" &&
+		shift 2 &&
+		true
+	    ;;
+	--report-organization)
+	    REPORT_ORGANIZATION="${2}" &&
+		shift 2 &&
+		true
+	    ;;
+	--report-repository)
+	    REPORT_REPOSITORY="${2}" &&
+		shift 2 &&
+		true
+	    ;;
+	*)
+	    echo Unknown Option &&
+		echo ${1} &&
+		echo ${0} &&
+		echo ${@} &&
+		exit 65 &&
+		true
+	    ;;
+    esac &&
+	true
+done &&
+    (cat <<EOF
+UPSTREAM_ORGANIZATION
+UPSTREAM_REPOSITORY
+ORIGIN_ORGANIZATION
+ORIGIN_REPOSITORY
+REPORT_ORGANIZATION
+REPORT_REPOSITORY
+EOF
+    ) | while read VAR do
+    do
+	eval VAL=\${${VAR}} &&
+	    if [ -z "${VAL}" ]
+	    then
+		echo Undefined ${VAR} &&
+		    echo ${0} &&
+		    exit 66 &&
+		    true
+	    fi &&
+	    true
+    done &&
+    if [ ! -d ${HOME}/.password-store ]
+    then
+	pass init $(gpg --list-keys --with-colon | head --lines 5 | tail --lines 1 | cut --fields 5 --delimiter ":") &&
+	    pass git init &&
+	    pass git remote add upstream "upstream:${UPSTREAM_ORGANIZATION}/${UPSTREAM_REPOSITORY}.git" &&
+	    pass git remote set-url --push upstream no-push &&
+	    pass git remote add origin "origin:${ORIGIN_ORGANIZATION}/${ORIGIN_REPOSITORY}.git" &&
+	    pass git remote add origin "origin:${REPORT_ORGANIZATION}/${REPORT_REPOSITORY}.git" &&
+	    ln \
+		--symbolic \
+		${STORE_DIR}/scripts/post-commit \
+		${STORE_DIR}/scripts/pre-push \
+		${HOME}/.password-store/.git/hooks &&
+	    true
+    fi &&
+    true
+    
