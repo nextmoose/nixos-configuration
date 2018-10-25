447bab6ad526b5f308fcbeb48b8c723bdbe23728
Emory Merryman <emory.merryman@gmail.com>
2018-10-25 17:25:56 -0400

    install containers and virtualization

--- install.sh
+++ install.sh
@@ -7,13 +7,27 @@ pvcreate /dev/sda4 &&
     DIR=$(mktemp -d) &&
     mount /dev/volumes/gnucash ${DIR} &&
     mkdir ${DIR}/user &&
-    chown 1000:100 ${DIR}/user &&
-    (cat > /mnt/etc/nixos/custom/containers.nix <<EOF
+    chown 1000:100 ${DIR}/user &
+if [ -d /root/mnt/configuration/custom ]
+then
+    ls -1 /root/mnt/configuration/custom | grep "[.]d\$" | while read DOMAIN
+    do
+	(cat > /root/mnt/configuration/custom/${DOMAIN%.*}.nix <<EOF
 { pkgs ? import <nixpkgs> {} }:
 with import <nixpkgs> {};
 {
+EOF
+	) &&
+	    ls -1 /root/mnt/configuration/custom/${DOMAIN} | grep "[.]nix\$" | while read SUB
+	    do
+		echo "  ${SUB%.*} = (import ./${DOMAIN%.*}.d/${SUB%.*}.nix { inherit pkgs; });" >> /root/mnt/configuration/custom/${DOMAIN%.*}.nix
+		true
+	    done &&
+	    (cat >> /root/mnt/configuration/custom/${DOMAIN%.*}.nix && <<EOF
 }
 EOF
-    ) &&
-    cp /mnt/etc/nixos/custom/containers.nix /mnt/etc/nixos/custom/virtualisation.nix &&
+	    ) &&
+	    true
+    done &&
+fi &&
     true
