051e5bb640dc3681c53d25f90fb5b3c5360bcb30
Emory Merryman <emory.merryman@gmail.com>
2018-10-25 14:35:40 -0400

    fix error in install script

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ pvcreate /dev/sda4 &&
     lvcreate --name gnucash --size 1G volumes &&
     mkfs.ext4 /dev/volumes/gnucash &&
     DIR=$(mktemp -d) &&
-    mount /dev/volumes/gnucash &&
+    mount /dev/volumes/gnucash ${DIR} &&
     mkdir ${DIR}/user &&
     chown 1000:100 ${DIR}/user &&
     touch /mnt/etc/nixos/containers.nix &&
