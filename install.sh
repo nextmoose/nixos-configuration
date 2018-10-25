5c1a36b1ed7ec829cc3e0fd81be1ab9c4ee0c34a
Emory Merryman <emory.merryman@gmail.com>
2018-10-25 12:53:16 -0400

    add install script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+pvcreate /dev/sda4 &&
+    vgcreate volumes /dev/sda4 &&
+    lvcreate --name gnucash --size 1G volumes &&
+    mkfs.ext4 /dev/volumes/gnucash &&
+    DIR=$(mktemp -d) &&
+    mount /dev/volumes/gnucash &&
+    mkdir ${DIR}/user &&
+    chown 1000:100 ${DIR}/user &&
+    touch /mnt/etc/nixos/containers.nix &&
+    touch /mnt/etc/nixos/virtualisation.nix &&
+    true
