1201814d3bdbf6a73c876e401c840b5f4482e10c
Emory Merryman <emory.merryman@gmail.com>
2018-10-26 10:02:54 -0400

    fix double ampersand error

--- install.sh
+++ install.sh
@@ -7,27 +7,28 @@ pvcreate /dev/sda4 &&
     DIR=$(mktemp -d) &&
     mount /dev/volumes/gnucash ${DIR} &&
     mkdir ${DIR}/user &&
-    chown 1000:100 ${DIR}/user &
-if [ -d /root/mnt/configuration/custom ]
-then
-    ls -1 /root/mnt/configuration/custom | grep "[.]d\$" | while read DOMAIN
-    do
-	(cat > /root/mnt/configuration/custom/${DOMAIN%.*}.nix <<EOF
-{ pkgs ? import <nixpkgs> {} }:
-with import <nixpkgs> {};
-{
-EOF
-	) &&
-	    ls -1 /root/mnt/configuration/custom/${DOMAIN} | grep "[.]nix\$" | while read SUB
-	    do
-		echo "  ${SUB%.*} = (import ./${DOMAIN%.*}.d/${SUB%.*}.nix { inherit pkgs; });" >> /root/mnt/configuration/custom/${DOMAIN%.*}.nix
-		true
-	    done &&
-	    (cat >> /root/mnt/configuration/custom/${DOMAIN%.*}.nix && <<EOF
-}
+    chown 1000:100 ${DIR}/user &&
+    if [ -d /root/mnt/configuration/custom ]
+    then
+	ls -1 /root/mnt/configuration/custom | grep "[.]d\$" | while read DOMAIN
+	do
+	    (cat > /root/mnt/configuration/custom/${DOMAIN%.*}.nix <<EOF
+{ pkgs ? impor	 t <nixpkgs> {} }:
+with import <nixp  kgs> {};
+{    
 EOF
 	    ) &&
+		ls -1 /root/mnt/configuration/custom/${DOMAIN} | grep "[.]nix\$" | while read SUB
+		do
+		    echo "  ${SUB%.*} = (import ./${DOMAIN%.*}.d/${SUB%.*}.nix { inherit pkgs; });" >> /root/mnt/configuration/custom/${DOMAIN%.*}.nix
+		    true
+		done &&
+		(cat >> /root/mnt/configuration/custom/${DOMAIN%.*}.nix && <<EOF
+}
+EOF
+		) &&
+		true
+	done &&
 	    true
-    done &&
-fi &&
+    fi &&
     true
