6dd73f304fbc38430fd6eb08cb91493117113fd8
Emory Merryman <emory.merryman@gmail.com>
2018-10-26 19:35:15 -0400

    do it

--- custom/expressions/container-initializations/src/gnucash.sh
+++ custom/expressions/container-initializations/src/gnucash.sh
@@ -5,6 +5,11 @@ secrets \
     --canonical-organization nextmoose \
     --canonical-repository secrets \
     --canonical-branch master &&
+    ssh-remote \
+	--name upstream \
+	--host github.com \
+	--user git \
+	--port 22 &&
     read-only-pass \
 	--upstream-organization nextmoose \
 	--upstream-repository aws-secrets \
