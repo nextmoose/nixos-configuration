4dd5e6a6b574a93d408f54dcca2253a73080dd83
Emory Merryman <emory.merryman@gmail.com>
2018-10-27 12:18:24 -0400

    add debucket

--- custom/expressions/backup-utils/default.nix
+++ custom/expressions/backup-utils/default.nix
@@ -16,6 +16,7 @@ stdenv.mkDerivation rec {
       makeWrapper $out/scripts/dearchive.sh $out/bin/dearchive --set PATH ${lib.makeBinPath [ "/run/wrappers" coreutils utillinux lvm2 gnutar gzip cdrkit cdi2iso 	mdf2iso ccd2iso libisofs dvdisaster gnupg awscli ]} &&
       makeWrapper $out/scripts/archive.sh $out/bin/archive --set PATH ${lib.makeBinPath [ "/run/wrappers" coreutils utillinux lvm2 gnutar gzip cdrkit cdi2iso 	mdf2iso ccd2iso libisofs dvdisaster gnupg awscli ]} &&
       makeWrapper $out/scripts/bucket.sh $out/bin/bucket --set PATH ${lib.makeBinPath [ "/run/wrappers" coreutils utillinux lvm2 gnutar gzip cdrkit cdi2iso mdf2iso ccd2iso libisofs dvdisaster gnupg awscli initialization-utils ]} &&
+      makeWrapper $out/scripts/debucket.sh $out/bin/debucket --set PATH ${lib.makeBinPath [ "/run/wrappers" coreutils utillinux lvm2 gnutar gzip cdrkit cdi2iso mdf2iso ccd2iso libisofs dvdisaster gnupg awscli initialization-utils ]} &&
       makeWrapper $out/scripts/snapshot.sh $out/bin/snapshot --set PATH ${lib.makeBinPath [ "/run/wrappers" coreutils utillinux lvm2 rsync ]} &&
       makeWrapper $out/scripts/test.sh $out/bin/test-backup-utils --set PATH ${lib.makeBinPath [ "/run/wrappers" coreutils utillinux lvm2 rsync "$out" e2fsprogs gnupg bash diffutils ]} &&
       true
