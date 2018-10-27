28acfe2818abb7376cfa67d69972d102f301f79c
Emory Merryman <emory.merryman@gmail.com>
2018-10-27 11:37:10 -0400

    adding comments about gnucash bullshit

--- custom/expressions/container-initializations/src/gnucash.sh
+++ custom/expressions/container-initializations/src/gnucash.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+#
+# gnucash has some weird kludge going on.
+#
+# 1. start `gnucash`
+# 2. there is some bullshit error - I don't know.  just click the defaults.  wait for gnucash to start.  then quit it
+# 3. sleep 3 minutes `sleep 3m`.  I don't know 3 minutes is necessary but it seems some sleep is required.  I tested on 3 minutes.  most likely less is necessary.
+# 4. gnucash should now work without the bullshit error `gnucash`
+# 5. initially you will be prompted to create a file.
+#
 secrets \
     --canonical-host github.com \
     --canonical-organization nextmoose \
