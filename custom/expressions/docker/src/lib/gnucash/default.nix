3d01808b83c9ed5c9d2b6560f41691fdfd190e37
Emory Merryman <emory.merryman@gmail.com>
2018-10-30 21:45:04 -0400

    do it

--- custom/expressions/docker/src/lib/gnucash/default.nix
+++ custom/expressions/docker/src/lib/gnucash/default.nix
@@ -3,11 +3,12 @@ let
   fedora = pkgs.dockerTools.pullImage {
     imageName = "fedora";
     imageTag = "29";
+    digest = "fedora@sha256:5c2fbd86f7de07ae71d85e7827b0c22180b9b83974d836cb105e09ec26627ec3";
     sha256 = "16nc7sl0kr3z5gz094h2a26mn0wd7s7vsql95zvljc1zrw7aai96";
   };
   entrypoint = pkgs.writeScript "entrypoint.sh" ''
      #!${pkgs.stdenv.shell}
-     ${pkgs.gnucash}/bin/gnucash
+     ${pkgs.gnucash}/bin/gn
   '';
 in
 with pkgs;
@@ -17,7 +18,8 @@ dockerTools.buildImage {
   fromImageName = "fedora";
   fromImageTag = "29";
   runAsRoot = ''
-    echo hello world
+    dnf update --assumeyes &&
+      true
   '';
   config = {
     Cmd = [ ];
