26757491ed81aa58cacc84374392c517d726fba5
Emory Merryman <emory.merryman@gmail.com>
2018-11-01 16:20:35 -0400

    timeout or true

--- custom/expressions/container-initializations/src/gnucash-2.sh
+++ custom/expressions/container-initializations/src/gnucash-2.sh
@@ -23,7 +23,7 @@ xml:readwrite:\$(HOME)/.gconf
 xml:readonly:/nix/store/p3wdpwf9aaqvr7qxhwmk3cn8lfdk089v-gnucash-2.4.15/etc/gconf/gconf.xml.defaults
 EOF
     ) &&
-    sleep 10s &&
+    sleep 1m &&
     echo &&
     echo BUCKET=${BUCKET} &&
     echo ${AWS_PATH}/bin/aws s3 ls s3://${BUCKET} &&
