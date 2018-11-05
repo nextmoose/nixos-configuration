0130a730c7e208d5ce1c23fc2fc080a417f8ddc7
Emory Merryman <emory.merryman@gmail.com>
2018-11-05 14:29:23 -0500

    add gnucash-3

---
+++ custom/containers.d/gnucash-3.nix
@@ -0,0 +1,20 @@
+{ pkgs ? import <nixpkgs> {} }:
+with import <nixpkgs> {};
+let
+  container-initializations = (import ../expressions/container-initializations/default.nix { inherit pkgs; });
+in
+{
+  config = { config, pkgs, ...}:
+  {
+    environment.variables = {
+      BUCKET="e613b6bb-3d0b-4d02-8af9-6b05a3c89d3e";
+      DISPLAY=":0";
+    };
+    services.mingetty.autologinUser = "user";
+    programs.bash.shellInit = "${container-initializations}/bin/gnucash-3"
+    users.extraUsers.user = {
+      isNormalUser = true;
+    };
+  };
+  tmpfs = [ "/home" ];
+}
---
+++ custom/expressions/container-initializations/src/gnucash-3.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+secrets \
+    --canonical-host github.com \
+    --canonical-organization nextmoose \
+    --canonical-repository secrets \
+    --canonical-branch master &&
+    ssh-remote \
+	--remote upstream \
+	--host github.com \
+	--user git \
+	--port 22 &&
+    read-only-pass \
+	--upstream-organization nextmoose \
+	--upstream-repository aws-secrets \
+	--upstream-branch master &&
+    aws \
+	--aws-access-key-id AKIAICSO2M2FPGDMRHNA \
+	--default-region-name us-east-1 \
+	--default-output-format json &&
+    debucket --
+    gnucash --gnucash gnucash/gnucash.gnucash &&
+    true
