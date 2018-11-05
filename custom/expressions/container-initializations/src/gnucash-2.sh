6fd8cb374fbfcef611f8a367dca27108875e8dd9
Emory Merryman <emory.merryman@gmail.com>
2018-11-05 12:25:25 -0500

    before

--- custom/expressions/container-initializations/src/gnucash-2.sh
+++ custom/expressions/container-initializations/src/gnucash-2.sh
@@ -19,7 +19,7 @@ secrets \
 	--default-region-name us-east-1 \
 	--default-output-format json &&
     ${AWS_PATH}/bin/aws s3 ls s3://${BUCKET} &&
-    TSTAMP=$(${AWS_PATH}/bin/aws s3 ls s3://${BUCKET} | sort | head --lines 1 | cut --bytes 40-49) &&
+    TSTAMP=$(${AWS_PATH}/bin/aws s3 ls s3://${BUCKET} | sort --reverse | head --lines 1 | cut --bytes 40-49) &&
     debucket --name gnucash --timestamp "${TSTAMP}" --destination-directory gnucash &&
     gnucash --gnucash gnucash/gnucash.gnucash &&
     true
