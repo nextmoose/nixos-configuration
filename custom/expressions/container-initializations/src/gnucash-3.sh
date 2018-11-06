2fe65b497617241b336a010046f929a022f72263
Emory Merryman <emory.merryman@gmail.com>
2018-11-05 20:46:29 -0500

    before

--- custom/expressions/container-initializations/src/gnucash-3.sh
+++ custom/expressions/container-initializations/src/gnucash-3.sh
@@ -18,6 +18,6 @@ secrets \
 	--aws-access-key-id AKIAICSO2M2FPGDMRHNA \
 	--default-region-name us-east-1 \
 	--default-output-format json &&
-    restore --name gnucash --bucket e613b6bb-3d0b-4d02-8af9-6b05a3c89d3e &&
+    restore --name gnucash --bucket e613b6bb-3d0b-4d02-8af9-6b05a3c89d3e --destination-directory gnucash &&
     gnucash --gnucash gnucash/gnucash.gnucash &&
     true
