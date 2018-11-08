31e5390e2ff9bdc5bd5456d401b92c4a300cc68e
Emory Merryman <emory.merryman@gmail.com>
2018-11-07 20:47:19 -0500

    before

---
+++ custom/containers.d/stormysteel.nix
@@ -0,0 +1,37 @@
+{ pkgs ? import <nixpkgs> {} }:
+with import <nixpkgs> {};
+let
+  container-initializations = (import ../expressions/container-initializations/default.nix { inherit pkgs; });
+in
+{
+  bindMounts = {
+    "/run/user/1000" = {
+      hostPath = "/run/user/1000";
+      isReadOnly = true;
+    };
+    "/home/user/.config/pulse" = {
+      hostPath = "/home/user/.config/pulse";
+      isReadOnly = true;
+    };
+  };
+  config = { config, pkgs, ...}:
+  {
+    environment.variables.DISPLAY=":0";
+    hardware.pulseaudio.enable = true;
+    programs.bash.shellInit = "${container-initializations}/bin/stormysteel";
+    services.mingetty.autologinUser = "user";
+    users.extraUsers.user = {
+      isNormalUser = true;
+      packages = [
+	pkgs.git
+	pkgs.emacs
+	pkgs.atom
+	pkgs.nodejs
+	pkgs.chromium
+	pkgs.firefox
+	pkgs.lynx
+      ];
+    };
+  };
+#  tmpfs = [ "/home" ];
+}
