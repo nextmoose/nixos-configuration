cd83cf6874821839240e1d9b2c9d00cf6a8672e6
Emory Merryman <emory.merryman@gmail.com>
2018-11-12 22:58:26 -0500

    before

--- custom/native/development/default.nix
+++ custom/native/development/default.nix
@@ -13,9 +13,9 @@ stdenv.mkDerivation rec {
       mkdir $out/bin &&
       makeWrapper $out/scripts/development.sh $out/bin/development --set PATH ${lib.makeBinPath [ coreutils pass gnupg git bash bash-completion openssh emacs ]} --set STORE_DIR "$out" &&
       makeWrapper $out/scripts/git-curt.sh $out/bin/git-curt --set PATH ${lib.makeBinPath [ git ] &&
-      makeWrapper $out/scripts/git-standing.sh $out/bin/git-standing --set PATH ${lib.makeBinPath [ git ] &&
-      makeWrapper $out/scripts/git-refresh.sh $out/bin/git-refresh --set PATH ${lib.makeBinPath [ git ] &&
-      makeWrapper $out/scripts/git-prepare.sh $out/bin/git-prepare --set PATH ${lib.makeBinPath [ git ] &&
+      makeWrapper $out/scripts/git-standing.sh $out/bin/git-standing --set PATH ${lib.makeBinPath [ git ]} &&
+      makeWrapper $out/scripts/git-refresh.sh $out/bin/git-refresh --set PATH ${lib.makeBinPath [ git ]} &&
+      makeWrapper $out/scripts/git-prepare.sh $out/bin/git-prepare --set PATH ${lib.makeBinPath [ git ]} &&
       makeWrapper $out/scripts/post-commit.sh $out/scripts/post-commit --set PATH ${lib.makeBinPath [ coreutils git ]} &&
       true
   '';
