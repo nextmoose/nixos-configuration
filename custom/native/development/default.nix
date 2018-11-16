562cf18ca8e780e640a9458af66f99144478b7ca
Emory Merryman <emory.merryman@gmail.com>
2018-11-16 12:54:01 -0500

    before

--- custom/native/development/default.nix
+++ custom/native/development/default.nix
@@ -11,7 +11,7 @@ stdenv.mkDerivation rec {
       cp --recursive scripts $out/scripts &&
       chmod 0500 $out/scripts/* &&
       mkdir $out/bin &&
-      makeWrapper $out/scripts/development.sh $out/bin/development --set PATH ${lib.makeBinPath [ coreutils pass gnupg git bash bash-completion openssh emacs "$out" utillinux maven which nodejs-slim-9.11.2 less ]} --set STORE_DIR "$out" &&
+      makeWrapper $out/scripts/development.sh $out/bin/development --set PATH ${lib.makeBinPath [ coreutils pass gnupg git bash bash-completion openssh emacs "$out" utillinux maven which nodejs-slim-9 less ]} --set STORE_DIR "$out" &&
       makeWrapper $out/scripts/git-curt.sh $out/bin/git-curt --set PATH ${lib.makeBinPath [ git ]} &&
       makeWrapper $out/scripts/git-standing.sh $out/bin/git-standing --set PATH ${lib.makeBinPath [ git ]} &&
       makeWrapper $out/scripts/git-refresh.sh $out/bin/git-refresh --set PATH ${lib.makeBinPath [ git "$out" utillinux ]} &&
