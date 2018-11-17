739323864d10183ffbd1730062fd774f48c278bd
Emory Merryman <emory.merryman@gmail.com>
2018-11-17 09:45:17 -0500

    this might work

--- custom/configuration-utils/default.nix
+++ custom/configuration-utils/default.nix
@@ -1,17 +1,17 @@
-{ pkgs ? import <nixpkgs> {} }:
-with import <nixpkgs> {};
-stdenv.mkDerivation rec {
-  name = "configuration-utils";
-  src = ./src;
-  buildInputs = [ makeWrapper which ];
-  installPhase = ''
-    mkdir $out &&
-      cp --recursive scripts $out &&
-      chmod 0500 $out/scripts/*.sh &&
-      mkdir $out/bin &&
-      which makeWrapper &&
-      makeWrapper $out/scripts/build-phase.sh $out/bin/build-phase --set PATH ${lib.makeBinPath [ coreutils which ]} &&
-      makeWrapper $out/scripts/install-phase.sh $out/bin/install-phase --set PATH ${lib.makeBinPath [ bash coreutils ]} &&
-      true
-  '';
+{ stdenv }:
+rec {
+  emory = {
+  ...
+  }:
+  let
+    foo = "bar";
+  in
+    stdenv.mkDerivation {
+      name = "${foo}";
+      src = ./src;
+      installPhase = ''
+        mkdir $out &&
+	  true
+      '';
+    };
 }
--- custom/hello/default.nix
+++ custom/hello/default.nix
@@ -1,16 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
 with import <nixpkgs> {};
 let
-  configuration-utils = (import ../configuration-utils/default.nix { inherit pkgs; });
+  configuration-utils = (import ../configuration-utils/default.nix { stdenv = pkgs.stdenv; });
 in
-stdenv.mkDerivation rec {
-  name = "hello";
-  src = ./src;
-  buildInputs = [ configuration-utils makeWrapper ];
-  buildPhase = "build-phase --build-dir build";
-  installPhase = ''
-    install-phase --build-dir build --install-dir $out &&
-      makeWrapper $out/scripts/hello.sh $out/bin/hello --set PATH ${lib.makeBinPath [ coreutils ]} &&
-      true
-  '';
+configuration-utils.emory {
 }
