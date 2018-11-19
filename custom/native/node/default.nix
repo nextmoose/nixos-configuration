728b22457a5cbb33c2f7e454f8fac7747417739a
Emory Merryman <emory.merryman@gmail.com>
2018-11-19 17:48:14 -0500

    fuck this

--- custom/native/node/default.nix
+++ custom/native/node/default.nix
@@ -2,9 +2,8 @@
 pkgs.stdenv.mkDerivation rec {
   name = "node";
   src = pkgs.fetchurl {
-    url = "https://nodejs.org/dist/v11.2.0/node-v11.2.0.tar.gz";
+    url = "https://nodejs.org/dist/latest-v8.x/node-v8.12.0-linux-x64.tar.gz";
     sha512 = "1vajl0dm5rjgmw46i6iz6jbkdya9c7vag9w82fmhl7wnw52d3zm1g1f8yawdn4na111j2rjnjls47w1ilqhpv2kibz8xvfbwp2zf57m";
   };
-  buildInputs = [ pkgs.which pkgs.python ];
-  installPhase = "make install PREFIX=$out";
+  installPhase = "cp --recursive . $out";
 }
