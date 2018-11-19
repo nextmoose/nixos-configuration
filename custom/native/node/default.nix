296f1bd187de2a32a08d1641c4ea9d0ef2667ed6
Emory Merryman <emory.merryman@gmail.com>
2018-11-19 17:18:53 -0500

    use PREFIX

--- custom/native/node/default.nix
+++ custom/native/node/default.nix
@@ -6,4 +6,5 @@ pkgs.stdenv.mkDerivation rec {
     sha512 = "1vajl0dm5rjgmw46i6iz6jbkdya9c7vag9w82fmhl7wnw52d3zm1g1f8yawdn4na111j2rjnjls47w1ilqhpv2kibz8xvfbwp2zf57m";
   };
   buildInputs = [ pkgs.which pkgs.python ];
+  installPhase = "make install PREFIX=$out";
 }
