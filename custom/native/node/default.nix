c026ac9c49a5c679c0d480420d24970863d534cd
Emory Merryman <emory.merryman@gmail.com>
2018-11-19 16:04:16 -0500

    do it

--- custom/native/node/default.nix
+++ custom/native/node/default.nix
@@ -5,7 +5,7 @@ pkgs.stdenv.mkDerivation rec {
     url = "http://nodejs.org/dist/v11.2.0/node-v11.2.0.tar.gz";
     sha512 = "1vajl0dm5rjgmw46i6iz6jbkdya9c7vag9w82fmhl7wnw52d3zm1g1f8yawdn4na111j2rjnjls47w1ilqhpv2kibz8xvfbwp2zf57m";
   };
-  buildInputs = [ pkgs.gnutar which ];
+  buildInputs = [ pkgs.gnutar pkgs.which pkgs.python ];
   installPhase = ''
     tar --extract --gzip --file . --directory $out &&
       true
