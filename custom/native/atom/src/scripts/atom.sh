367d02d29b1581ff5e4728d8f51ea4168ce1a791
Emory Merryman <emory.merryman@gmail.com>
2018-11-21 14:00:43 -0500

    do it

--- custom/native/atom/src/scripts/atom.sh
+++ custom/native/atom/src/scripts/atom.sh
@@ -38,7 +38,7 @@ EOF
     ln --symbolic "$(which post-commit)" "${HOME}/project/.git/hooks" &&
     mkdir "${HOME}/.atom" &&
 #    cp "${STORE_DIR}/lib/atom" "${HOME}/.atom" &&
-    apm install atom-beautify prettier-atom atom-spotify2 atom-transpose case-keep-replace change-case copy-path duplicate-line-or-selection editorconfig file-icons git-plus highlight-selected local-history project-manager related set-syntax sort-lines sublime-style-column-selection tab-foldername-index sync-settings toggle-quotes atom-wrap-in-tag atom-ternjs autoclose-html autocomplete-modules color-picker docblockr emmet emmet-jsx-css-modules es6-javascript js-hyperclick hyperclick pigments linter-eslint tree-view-copy-relative-path lodash-snippets language-babel react-es7-snippets atom-jest-snippets one-dark-ui dracula-theme save-commands-plus &&
+    apm install save-commands-plus &&
     cp "${STORE_DIR}/lib/atom/." project &&
     atom --foreground "${HOME}/project" &&
     true
