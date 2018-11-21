3b7c13912d8cda3b229122ed4b1fec3992df5aab
Emory Merryman <emory.merryman@gmail.com>
2018-11-21 13:29:36 -0500

    try to install shit

--- custom/native/atom/src/scripts/atom.sh
+++ custom/native/atom/src/scripts/atom.sh
@@ -36,8 +36,8 @@ EOF
     git -C "${HOME}/project" fetch origin "${ORIGIN_BRANCH}" &&
     git -C "${HOME}/project" checkout "${ORIGIN_BRANCH}" &&
     ln --symbolic "$(which post-commit)" "${HOME}/project/.git/hooks" &&
-    mkdir "${HOME}/.atom" &&
     cp --recursive "${STORE_DIR}/lib/atom" "${HOME}/.atom" &&
-    apm install atom-beautify prettier-atom atom-spotify2 atom-transpose case-keep-replace change-case copy-path duplicate-line-or-selection editorconfig file-icons git-plus highlight-selected local-history project-manager related set-syntax sort-lines sublime-style-column-selection tab-foldername-index sync-settings toggle-quotes atom-wrap-in-tag atom-ternjs autoclose-html autocomplete-modules color-picker docblockr emmet emmet-jsx-css-modules es6-javascript js-hyperclick hyperclick pigments linter-eslint tree-view-copy-relative-path lodash-snippets language-babel react-es7-snippets atom-jest-snippets one-dark-ui dracula-theme &&
+    apm install atom-beautify prettier-atom atom-spotify2 atom-transpose case-keep-replace change-case copy-path duplicate-line-or-selection editorconfig file-icons git-plus highlight-selected local-history project-manager related set-syntax sort-lines sublime-style-column-selection tab-foldername-index sync-settings toggle-quotes atom-wrap-in-tag atom-ternjs autoclose-html autocomplete-modules color-picker docblockr emmet emmet-jsx-css-modules es6-javascript js-hyperclick hyperclick pigments linter-eslint tree-view-copy-relative-path lodash-snippets language-babel react-es7-snippets atom-jest-snippets one-dark-ui dracula-theme save-commands-plus &&
+    cp "${STORE_DIR}/lib/atom/." project &&
     atom --foreground "${HOME}/project" &&
     true
