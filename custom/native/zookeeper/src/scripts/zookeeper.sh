b44215ca350a1345e0a6bbbc0ada4b379bcc63c4
Emory Merryman <emory.merryman@gmail.com>
2018-11-24 15:12:30 -0500

    it workds

--- custom/native/zookeeper/src/scripts/zookeeper.sh
+++ custom/native/zookeeper/src/scripts/zookeeper.sh
@@ -2,6 +2,5 @@
 
 cp ${STORE_DIR}/lib/zookeeper.conf ${HOME}/zookeeper.conf &&
     mkdir ${HOME}/zookeeper &&
-    echo zkServer.sh start-foreground ${HOME}/zookeeper.conf &&
     zkServer.sh start-foreground ${HOME}/zookeeper.conf &&
     true
