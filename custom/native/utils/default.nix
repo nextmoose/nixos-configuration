03d73cb6f4f788bed7e3fa319602c28868cbe5d1
Emory Merryman <emory.merryman@gmail.com>
2018-11-25 13:31:59 -0500

    do it

--- custom/native/utils/default.nix
+++ custom/native/utils/default.nix
@@ -13,6 +13,12 @@ pkgs.stdenv.mkDerivation rec {
 	$out/bin/docker-system-prune \
 	--set PATH ${pkgs.lib.makeBinPath [ pkgs.docker pkgs.coreutils ]} \
 	--set STORE_DIR "$out" &&
+      true &&
+      makeWrapper \
+        $out/scripts/update-system.sh \
+	$out/bin/docker-system-prune \
+	--set PATH ${pkgs.lib.makeBinPath [ "/run/wrappers" "/run/current-system/sw" pkgs.coreutils pkgs.rsync pkgs.mktemp pkgs.nix pkgs.docker ]} \
+	--set STORE_DIR "$out" &&
       true
   '';
 }
---
+++ custom/native/utils/src/scripts/update-system.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+GARBAGE="no" &&
+    DOCKER="no" &&
+    while [ "${#}" -gt 0 ]
+    do
+	case "${1}" in
+	    --source-directory)
+		SOURCE_DIRECTORY="${2}" &&
+		    shift 2 &&
+		    true
+		;;
+	    --garbage)
+		GARBAGE="yes" &&
+		    shift 1 &&
+		    true
+		;;
+	    --docker)
+		DOCKER="yes" &&
+		    shift 1 &&
+		    true
+		;;
+	    *)
+		echo Unknown Option &&
+		    echo "${1}" &&
+		    echo "${0}" &&
+		    echo "${@}" &&
+		    exit 66 &&
+		    true
+		;;
+	esac
+    done &&
+    TEMP_DIR=$(mktemp -d) &&
+    cleanup() {
+	rm --recursive --force ${TEMP_DIR} &&
+	    true
+    } &&
+    cd "${SOURCE_DIRECTORY}" &&
+    if [ "${GARBAGE}" == "yes" ]
+    then
+	nix-collect-garbage &&
+	    true
+    fi &&
+    if [ "${DOCKER}" == "yes" ]
+    then
+	docker container ls --quiet --all | while read CID
+	do
+	    docker container stop "${CID}" &&
+		docker container rm "${CID}" &&
+		true
+	done &&
+	    docker system prune --force --all --volumes &&
+	    true
+    fi &&
+    if [ -f configuration.nix ]
+    then
+	sudo cp configuration.nix /etc/nixos
+    fi &&
+    if [ -d custom ]
+    then
+	rsync \
+	    --verbose \
+	    --archive \
+	    --delete \
+	    custom \
+	    /etc/nixos &&
+	    true
+    fi &&
+    sudo nixos-rebuild switch &&
+    true
