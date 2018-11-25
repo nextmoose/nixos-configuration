5c9b7474236ed7d23cffad0a3b3c2cdaae9bcf49
Emory Merryman <emory.merryman@gmail.com>
2018-11-25 17:01:55 -0500

    garbage

--- custom/native/kafka/src/scripts/kafka.sh
+++ custom/native/kafka/src/scripts/kafka.sh
@@ -19,28 +19,31 @@ TOPICS=() &&
 		LIST_TOPICS="yes" &&
 		    shift &&
 		    true
-		*)
-		  echo Unknown Option &&
-		      echo "${1}" &&
-		      echo "${0}" &&
-		      echo "${@}" &&
-		      exit 66 &&
-		      true
-		  ;;
-		  esac &&
-		      true
-done &&
-		      sed \
-			  -e "s#broker.id=0#broker.id=${BROKER_ID}#" \
-			  -e "w${HOME}/server.conf" ${STORE_DIR}/lib/server.conf && 
-		      kafka-server-start.sh "${HOME}/server.conf" &&
-		      for TOPIC in ${TOPICS[*]}
-		      do
-			  kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic ${TOPIC} &&
-			      true
-		      done &&
-		      if [ "${LIST_TOPICS}" == "yes" ]
-		      then
-			  kafka-topics.sh --list --zookeeper zookeeper:2181
-		      fi &&
-		      true
+		;;
+	    *)
+		echo Unknown Option &&
+		    echo "${1}" &&
+		    echo "${0}" &&
+		    echo "${@}" &&
+		    exit 66 &&
+		    true
+		;;
+	esac &&
+	    true
+    done &&
+    sed \
+	-e "s#broker.id=0#broker.id=${BROKER_ID}#" \
+	-e "w${HOME}/server.conf" ${STORE_DIR}/lib/server.conf && 
+    kafka-server-start.sh "${HOME}/server.conf" &&
+    for TOPIC in ${TOPICS[*]}
+    do
+	kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic ${TOPIC} &&
+	    true
+    done &&
+    if [ "${LIST_TOPICS}" == "yes" ]
+    then
+	echo THIS IS A LIST OF THE TOPICS &&
+	    kafka-topics.sh --list --zookeeper zookeeper:2181 &&
+	    true
+    fi &&
+    true
