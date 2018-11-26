35801532aa696648d80eecc293b18f34be4e9bf1
Emory Merryman <emory.merryman@gmail.com>
2018-11-26 12:53:26 -0500

    fix canonical branch

--- custom/native/utilities/src/scripts/init-read-only-pass.sh
+++ custom/native/utilities/src/scripts/init-read-only-pass.sh
@@ -3,7 +3,7 @@
 import-gnupg-keys &&
     pass init "$(gnupg-key-id)" &&
     pass git init &&
-    pass git remote add canonical "https://${CANONICAL_HOST}:${CANONICAL_ORGANIZATION}/${CANONICAL_REPOSITORY}.git" &&
+    pass git remote add canonical "https://${CANONICAL_HOST}/${CANONICAL_ORGANIZATION}/${CANONICAL_REPOSITORY}.git" &&
     pass git fetch canonical "${CANONICAL_BRANCH}" &&
     pass git checkout "canonical/${CANONICAL_BRANCH}" &&
     true
