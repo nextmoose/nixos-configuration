466e3259903654356c5e3dd7aa5edcabdd4e5f19
Emory Merryman <emory.merryman@gmail.com>
2018-11-26 16:11:16 -0500

    fixup

--- custom/temporary/secrets/src/scripts/export-env-vars.sh
+++ custom/temporary/secrets/src/scripts/export-env-vars.sh
@@ -3,5 +3,5 @@
 export GPG_SECRET_KEY="$(cat ${STORE_DIR}/lib/gpg.secret.key)" &&
     export GPG_OWNER_TRUST="$(cat ${STORE_DIR}/lib/gpg.owner.trust)" &&
     export GPG2_SECRET_KEY="$(cat ${STORE_DIR}/lib/gpg2.secret.key)" &&
-    export GPG2_OWNER_TRUST="$(cat ${STORE_DIR}/lib/gpg2xs.owner.trust)" &&
+    export GPG2_OWNER_TRUST="$(cat ${STORE_DIR}/lib/gpg2.owner.trust)" &&
     true
