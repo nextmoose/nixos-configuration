6e2191002a50b575f3654b6c572de558bb03f639
Emory Merryman <emory.merryman@gmail.com>
2018-11-26 22:40:07 -0500

    done

--- custom/user/beta-pass/src/scripts/pass.sh
+++ custom/user/beta-pass/src/scripts/pass.sh
@@ -3,23 +3,26 @@
 CONTAINER="$(docker container ls --quiet --filter label=uuid=${UUID})" &&
     if [ -z "${CONTAINER}" ]
     then
-	CONTAINER=$(docker \
-			container \
-			create \
-			--env GPG_SECRET_KEY="$(pass show gpg.secret.key)" \
-			--env GPG_OWNER_TRUST="$(pass show gpg.owner.trust)" \
-			--env GPG2_SECRET_KEY="$(pass show gpg2.secret.key)" \
-			--env GPG2_OWNER_TRUST="$(pass show gpg2.owner.trust)" \
-			--env ORIGIN_ID_RSA="$(pass show origin.id_rsa)" \
-			--env ORIGIN_KNOWN_HOSTS="$(pass show origin.known_hosts)" \
-			--env DISPLAY \
-			--env ORIGIN_HOST \
-			--env ORIGIN_ORGANIZATION \
-			--env ORIGIN_REPOSITORY \
-			--env ORIGIN_BRANCH \
-			--mount type=bind,source=/tmp/.X11-unix/X0,destination=/tmp/.X11-unix/X0,readonly=true \
-			--label=uuid=${UUID} \
-			read-only-pass) &&
+	pass show alpha &&
+	    sleep 1s &&
+	    CONTAINER=$(docker \
+			    container \
+			    create \
+			    --env GPG_SECRET_KEY="$(pass show gpg.secret.key)" \
+			    --env GPG_OWNER_TRUST="$(pass show gpg.owner.trust)" \
+			    --env GPG2_SECRET_KEY="$(pass show gpg2.secret.key)" \
+			    --env GPG2_OWNER_TRUST="$(pass show gpg2.owner.trust)" \
+			    --env ORIGIN_ID_RSA="$(pass show origin.id_rsa)" \
+			    --env ORIGIN_KNOWN_HOSTS="$(pass show origin.known_hosts)" \
+			    --env DISPLAY \
+			    --env ORIGIN_HOST \
+			    --env ORIGIN_ORGANIZATION \
+			    --env ORIGIN_REPOSITORY \
+			    --env ORIGIN_BRANCH \
+			    --mount type=bind,source=/tmp/.X11-unix/X0,destination=/tmp/.X11-unix/X0,readonly=true \
+			    --label=uuid=${UUID} \
+			    read-write-pass) &&
+	    sleep 1 &&
 	    docker container start "${CONTAINER}" &&
 	    true
     fi &&
