f4b8a2d93b3d34d36033258eff2307090b367423
Emory Merryman <>
2018-12-01 18:10:12 +0000

    empty

--- configuration.nix
+++ configuration.nix
@@ -84,7 +84,6 @@ in
       pkgs.recordmydesktop
       pkgs.chromium
       pkgs.git
-      (import <nixpkgs/master/pkgs/applications/networking/browsers/firefox-bin/default.nix> {})
     ];
   };
   virtualisation.docker = {
