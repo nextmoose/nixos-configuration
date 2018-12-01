c8a4bc6af2ebcbca146887275ac08cafd0682340
Emory Merryman <>
2018-12-01 19:47:07 +0000

    empty

--- custom/native/chromium/default.nix
+++ custom/native/chromium/default.nix
@@ -4,19 +4,7 @@
   version ? "44"
 }:
 let
-  depot-tools = pkgs.stdenv.mkDerivation {
-    name = "depot_tools";
-    src = pkgs.fetchgit {
-      url = "https://chromium.googlesource.com/chromium/tools/depot_tools.git";
-      rev = "61ea30737c65fb3a6ad7096f447465e58f8b915c";
-      sha256 = "1pxrafl4prxa064l1ijra1ybxd26gqlqf6whkmj5nfwsvldsrnin";
-    };
-    buildInputs = [ pkgs.makeWrapper ];
-    installPhase = ''
-      cp --recursive . $out &&
-        true
-    '';
-  };
+  foo = "bar";
 in
 pkgs.stdenv.mkDerivation {
   name = "${name}-${version}";
