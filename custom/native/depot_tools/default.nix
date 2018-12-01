40a8e84b6a682bea788982e0638cc6b771f67a6a
Emory Merryman <>
2018-12-01 12:45:44 +0000

    try this

--- custom/native/depot_tools/default.nix
+++ custom/native/depot_tools/default.nix
@@ -1,12 +1,12 @@
 { pkgs ? import <nixpkgs> {} }:
-stdenv.mkDerivation {
+pkgs.stdenv.mkDerivation {
   name = "depot_tools";
-  src = fetchgit {
+  src = pkgs.fetchgit {
     url = "https://chromium.googlesource.com/chromium/tools/depot_tools.git";
     rev = "Ic5029d6b12cdb6ea30704d90229ab7e365dc7273";
     sha256 = "1cw5fszffl5pkpa6s6wjnkiv6lm5k618s32sp60kvmvpy7a2v9kg";
   };
-  buildInputs = [ makeWrapper ];
+  buildInputs = [ pkgs.makeWrapper ];
   installPhase = ''
     cp --recursive . $out &&
       true
