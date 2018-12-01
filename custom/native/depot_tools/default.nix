e64acfed13a8b701e77bfd819bfeba68638ba5ed
Emory Merryman <>
2018-12-01 13:09:48 +0000

    try this

--- custom/native/depot_tools/default.nix
+++ custom/native/depot_tools/default.nix
@@ -3,7 +3,7 @@ pkgs.stdenv.mkDerivation {
   name = "depot_tools";
   src = pkgs.fetchgit {
     url = "https://chromium.googlesource.com/chromium/tools/depot_tools.git";
-    rev = "41ae473398b528215aa41f553e467e2473231b49";
+    rev = "61ea30737c65fb3a6ad7096f447465e58f8b915c";
     sha256 = "1cw5fszffl5pkpa6s6wjnkiv6lm5k618s32sp60kvmvpy7a2v9kg";
   };
   buildInputs = [ pkgs.makeWrapper ];
