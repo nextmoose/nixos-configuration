2b0e8759f93d0a5638c8422ded0592bb8aa4710b
Emory Merryman <>
2018-12-03 16:43:50 +0000

    try this fix 11

--- custom/native/initialization/src/scripts/initialization.sh
+++ custom/native/initialization/src/scripts/initialization.sh
@@ -7,12 +7,12 @@
     done &&
     nmcli device wifi list | grep "^\s" | cut --bytes 1-29 | sed -e "s#^\s*##" -e "s#\s*\$##" | while read SSID
     do
-      case ${SSID} in
+      case "${SSID}" in
         "Richmond Sq Guest":
           nmcli device wifi connect "${SSID}" password guestwifi &&
             true
         ;;
-        "56LY"
+        "56LY":
           nmcli device wifi connect "${SSID}" password "$(pass show wifi/${SSID})" &&
             true
         ;;
