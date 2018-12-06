58b3906ce71da9e5de92e8ceacca39e13f9842fc
Emory Merryman <>
2018-12-06 19:16:33 +0000

    do it

---
+++ custom/initialization/gnupg/default.nix
@@ -0,0 +1,21 @@
+{pkgs ? import <nixpkgs> {} }:
+let
+  pass = (import ../../../installed/pass.nix {
+    pkgs = pkgs;
+  });
+pkgs.stdenv.mkDerivation {
+  name = "gnupg";
+  src = ./src;
+  buildInputs = [ pkgs.makeWrapper ];
+  installPhase = ''
+    mkdir $out &&
+      cp --recursive scripts $out &&
+      chmod 0500 $out/scripts/*.sh &&
+      mkdir $out/bin &&
+      makeWrapper \
+        $out/scripts/gnupg.sh \
+        $out/bin/gnupg \
+        --set PATH ${pkgs.lib.makeBinPath [ pass pkgs.gnupg pkgs.mktemp pkgs.coreutils ]} &&
+      true
+  '';
+};
