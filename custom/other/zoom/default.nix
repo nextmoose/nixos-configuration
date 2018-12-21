40fc735e1782e41aedae666c46d9c1c6360bdf71
Emory Merryman <emory.merryman@gmail.com>
2018-12-21 17:54:10 +0000

    fix it

--- custom/other/zoom/default.nix
+++ custom/other/zoom/default.nix
@@ -5,5 +5,6 @@ pkgs.stdenv.mkDerivation {
     url = "https://www.zoom.us/client/latest/zoom_x86_64.tar.xz";
     sha256 = "0bs5kx2601lwwr9lgdd3hlbrrwsf0dai766zrca907dl400pmzyd";
   };
+  buildRequires = [ pkgs.bash ];
   buildPhase = "./ZoomLauncher";
 }
