04683ab6adde32715220807758e0ee6be2978229
Emory Merryman <emory.merryman@gmail.com>
2018-12-21 20:21:42 +0000

    fix it

--- custom/scripts/dot-ssh-init/dot-ssh-init.sh
+++ custom/scripts/dot-ssh-init/dot-ssh-init.sh
@@ -3,7 +3,7 @@
 mkdir "${HOME}/.ssh" &&
     chmod 0700 "${HOME}/.ssh" &&
     (cat > "${HOME}/.ssh/config" <<EOF
-Include ${HOME}/.ssh/*.config
+Include ${HOME}/.ssh/*.conf
 EOF
     ) &&
     chmod 0400 "${HOME}/.ssh/config" &&
