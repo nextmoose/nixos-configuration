263deda6c35042eb16a7c0fc8c7e7ed14f25adf4
Emory Merryman <emory.merryman@gmail.com>
2018-12-21 17:29:24 +0000

    fix it

--- custom/other/zoom/default.nix
+++ custom/other/zoom/default.nix
@@ -3,6 +3,6 @@ pkgs.stdenv.mkDerivation {
   name = "zoom";
   src = pkgs.fetchurl {
     url = "https://www.zoom.us/client/latest/zoom_x86_64.tar.xz";
-    sha256 = "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824";
+    sha256 = "0bs5kx2601lwwr9lgdd3hlbrrwsf0dai766zrca907dl400pmzyd";
   };
 }
