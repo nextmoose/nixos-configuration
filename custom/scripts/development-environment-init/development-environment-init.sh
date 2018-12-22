b18c16cb3f75d285cc00ddfb31fb4889aad103f6
Emory Merryman <emory.merryman@gmail.com>
2018-12-22 14:59:38 +0000

    fix committer-email

--- custom/scripts/development-environment-init/development-environment-init.sh
+++ custom/scripts/development-environment-init/development-environment-init.sh
@@ -6,10 +6,12 @@ do
 	--committer-name)
 	    COMMITTER_NAME="${2}" &&
 		shift 2 &&
+		true
 	    ;;
-	--committer-name)
-	    COMMITTER_NAME="${2}" &&
+	--committer-email)
+	    COMMITTER_EMAIL="${2}" &&
 		shift 2 &&
+		true
 	    ;;
 	--upstream-host)
 	    UPSTREAM_HOST="${2}" &&
