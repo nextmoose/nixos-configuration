2e0fd4d2ce3f8da4e4965ee312026e43b94a0695
Emory Merryman <emory.merryman@gmail.com>
2018-12-22 23:54:58 +0000

    wtf

--- custom/utils/custom-npm-derivation.nix
+++ custom/utils/custom-npm-derivation.nix
@@ -9,7 +9,7 @@ pkgs.stdenv.mkDerivation {
    src = src;
    buildInputs = [ node ];
    buildPhase = ''
-     npm install &&
+     npm install --unsafe-perm &&
        true
    '';
    installPhase = ''
