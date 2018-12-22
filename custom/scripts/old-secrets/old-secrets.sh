06c7f569f0762897019eee721a20dbe9ff7c7ddf
Emory Merryman <emory.merryman@gmail.com>
2018-12-22 13:14:11 +0000

    add old secrets

--- custom/scripts/old-secrets/old-secrets.sh
+++ custom/scripts/old-secrets/old-secrets.sh
@@ -8,7 +8,7 @@ docker \
     --rm \
     --mount type=bind,source=/tmp/.X11-unix/X0,destination=/tmp/.X11-unix/X0,readonly=true \
     --env DISPLAY \
-    read-only-pass \
+    pass \
     --upstream-host github.com \
     --upstream-user git \
     --upstream-port 22 \
