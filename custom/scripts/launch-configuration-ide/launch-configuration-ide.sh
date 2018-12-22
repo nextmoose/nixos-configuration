892f4b76d6c96b24f9ff91368b14595809f14771
Emory Merryman <emory.merryman@gmail.com>
2018-12-22 13:51:44 +0000

    add launch-configuration ide

--- custom/scripts/launch-configuration-ide/launch-configuration-ide.sh
+++ custom/scripts/launch-configuration-ide/launch-configuration-ide.sh
@@ -7,19 +7,21 @@ docker \
     --mount type=bind,source=/tmp/.X11-unix/X0,destination=/tmp/.X11-unix/X0,readonly=true \
     --env DISPLAY \
     emacs \
+    --committer-name "Emory Merryman" \
+    --committer-email "emory.merryman@gmail.com" \
     --upstream-host github.com \
     --upstream-user git \
     --upstream-port 22 \
     --upstream-organization rebelplutonium \
     --upstream-repository nixos-configuration \
     --upstream-branch master \
-   --origin-host github.com \
+    --origin-host github.com \
     --origin-user git \
     --origin-port 22 \
     --origin-organization rebelplutonium \
     --origin-repository nixos-configuration \
     --origin-branch level-5 \
-   --report-host github.com \
+    --report-host github.com \
     --report-user git \
     --report-port 22 \
     --report-organization rebelplutonium \
