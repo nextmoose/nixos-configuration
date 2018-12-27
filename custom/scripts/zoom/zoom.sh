2f16c1e54123a4e1316ae5273137f96103251c60
Emory Merryman <emory.merryman@gmail.com>
2018-12-27 22:38:42 +0000

    add video device

--- custom/scripts/zoom/zoom.sh
+++ custom/scripts/zoom/zoom.sh
@@ -7,6 +7,7 @@ docker \
     --tty \
     --rm \
     --shm-size=256m \
+    --device /dev/video0 \
     --mount type=bind,source=/home/user,destination=/srv/home,readonly=false \
     --mount type=bind,source=/,destination=/srv/host,readonly=true \
     --mount type=bind,source=/tmp/.X11-unix/X0,destination=/tmp/.X11-unix/X0,readonly=true \
