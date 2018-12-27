b5e551dbfa91355d8a83df73f644024e726633e4
Emory Merryman <emory.merryman@gmail.com>
2018-12-27 22:31:58 +0000

    cribbed from urgemerge/chromium

--- custom/scripts/zoom/zoom.sh
+++ custom/scripts/zoom/zoom.sh
@@ -10,7 +10,7 @@ docker \
     --mount type=bind,source=/home/user,destination=/srv/home,readonly=false \
     --mount type=bind,source=/,destination=/srv/host,readonly=true \
     --mount type=bind,source=/tmp/.X11-unix/X0,destination=/tmp/.X11-unix/X0,readonly=true \
-    --mount type=bind,source=/run/user/1000/pulse:/run/user/1000/pulse,readonly=true \
+    --mount type=bind,source=/run/user/1000/pulse,destination=/run/user/1000/pulse,readonly=true \
     --mount type=bind,source=/etc/machine-id,destination=/etc/machine-id,readonly=true \
     --mount type=bind,source=/var/run/dbus/system_bus_socket,destination=/var/run/dbus/system_bus_socket,readonly=true \
     --mount type=bind,source=/home/user/.config/pulse,destination=/home/user/.config/pulse,readonly=true \
