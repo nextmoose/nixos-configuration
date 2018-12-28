d9c9a0b68448772a7df45586eac054e4f239a2f1
Emory Merryman <emory.merryman@gmail.com>
2018-12-28 16:25:46 +0000

    do it in 2 phases

--- custom/other/cloud9/default.nix
+++ custom/other/cloud9/default.nix
@@ -15,11 +15,12 @@ pkgs.stdenv.mkDerivation {
     sha256 = "1q3h3nhrip4bclm627n8k8g0jgpnfl840ipv8kphn4q413qzcyc7";
   };
   buildInputs = [ pkgs.wget pkgs.curl node pkgs.which pkgs.git ];
+  buildPhase = ''
+    ./scripts/install-sdk.sh &&
+  '';
   installPhase = ''
     mkdir $out &&
       cp --recursive . $out &&
-      cd $out &&
-      sh ./scripts/install-sdk.sh &&
       true
   '';
 }
