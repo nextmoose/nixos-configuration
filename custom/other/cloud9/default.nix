626215f1c16430954f07004a6e74ad4239e1d703
Emory Merryman <emory.merryman@gmail.com>
2018-12-28 22:06:31 +0000

    wtf

--- custom/other/cloud9/default.nix
+++ custom/other/cloud9/default.nix
@@ -26,6 +26,7 @@ pkgs.stdenv.mkDerivation {
       export GIT_SSL_NO_VERIFY=true &&
       export NODE_TLS_REJECT_UNAUTHORIZED=0 &&
       sh ./scripts/install-sdk.sh &&
+      echo FINISHED INSTALLING &&
       curl --output install.sh -L https://raw.githubusercontent.com/c9/install/master/install.sh &&
       echo ALPHA &&
       sh ./install.sh install &&
