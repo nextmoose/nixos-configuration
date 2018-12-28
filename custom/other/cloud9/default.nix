d71e184514b937518cb6df855910151b595c5946
Emory Merryman <emory.merryman@gmail.com>
2018-12-28 16:20:57 +0000

    install node

--- custom/other/cloud9/default.nix
+++ custom/other/cloud9/default.nix
@@ -1,6 +1,11 @@
 {
   pkgs ? import <nixpkgs> {},
 }:
+let
+  node = (import ../../native/node/default.nix {
+    pkgs = pkgs;
+  });
+in
 pkgs.stdenv.mkDerivation {
   name = "cloud9";
   src = pkgs.fetchFromGitHub {
@@ -9,7 +14,7 @@ pkgs.stdenv.mkDerivation {
     rev = "c4d1c59dc8d6619bdca3dbe740291cd5cd26352c";
     sha256 = "1q3h3nhrip4bclm627n8k8g0jgpnfl840ipv8kphn4q413qzcyc7";
   };
-  buildInputs = [ pkgs.wget pkgs.curl ];
+  buildInputs = [ pkgs.wget pkgs.curl node ];
   installPhase = ''
     mkdir $out &&
       cp --recursive . $out &&
