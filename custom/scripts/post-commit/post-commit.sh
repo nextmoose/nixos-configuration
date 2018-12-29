2b838c0f91467507f9d357b65eaa9b4ff79089e0
Emory Merryman <emory.merryman@gmail.com>
2018-12-29 17:16:44 +0000

    log post-commit

--- custom/scripts/post-commit/post-commit.sh
+++ custom/scripts/post-commit/post-commit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-while ! git push origin $(git rev-parse --abbrev-ref HEAD --)
+while ! git push origin $(git rev-parse --abbrev-ref HEAD --) >> /home/user/log.txt 2>&1
 do
     sleep 1s &&
 	true
