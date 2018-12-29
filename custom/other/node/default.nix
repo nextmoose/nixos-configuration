2f6cdf63ad81ba7068938d32f8b8deb0b25e27bd
Emory Merryman <emory.merryman@gmail.com>
2018-12-29 20:38:35 +0000

    wtf

--- custom/other/node/default.nix
+++ custom/other/node/default.nix
@@ -13,6 +13,7 @@ pkgs.stdenv.mkDerivation {
     mkdir $out/home/npm-packages &&
     export HOME=$out/home &&
     export NPM_PACKAGES=$out/home/npm-packages &&
+    echo OUT=$out &&
     ${node}/bin/npm install -g ${node-packages} &&
     makeWrapper \
       ${node}/bin/node \
