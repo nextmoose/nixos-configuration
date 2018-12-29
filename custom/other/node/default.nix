1d9d75b453ab318f43a6ad6fc64945bc2a8d3241
Emory Merryman <emory.merryman@gmail.com>
2018-12-29 20:57:46 +0000

    wtf

--- custom/other/node/default.nix
+++ custom/other/node/default.nix
@@ -10,6 +10,8 @@ pkgs.stdenv.mkDerivation {
   installPhase = ''
     mkdir $out &&
     mkdir $out/project &&
+    mkdir $out/home &&
+    export HOME=$out/home &&
     cd $out/project &&
     npm install ${node-packages} &&
     ${node}/bin/npm install -g ${node-packages} &&
