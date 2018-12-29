efefc253693d811744a629980dd6ce52f7f331e1
Emory Merryman <emory.merryman@gmail.com>
2018-12-29 14:06:32 +0000

    try this

--- custom/other/cloud9/default.nix
+++ custom/other/cloud9/default.nix
@@ -15,7 +15,7 @@ let
     pkgs = pkgs;
     name = "install-it";
     src = ./install;
-    dependencies = [ insecure-curl pkgs.which node pkgs.python pkgs.tmux pkgs.gnumake pkgs.coreutils pkgs.gcc pkgs.gnutar ];
+    dependencies = [ insecure-curl pkgs.which node pkgs.python pkgs.tmux pkgs.gnumake pkgs.coreutils pkgs.gcc pkgs.gnutar pkgs.gzip ];
   });
 in
 pkgs.stdenv.mkDerivation {
