1d2db2c5a9adc8eaaf3e158be065192d15f3ccc8
Emory Merryman <emory.merryman@gmail.com>
2018-12-30 18:26:43 +0000

    forgot to install

--- custom/scripts/npm-global-install/npm-global-install.sh
+++ custom/scripts/npm-global-install/npm-global-install.sh
@@ -14,6 +14,7 @@ mkdir "${HOME}/.npm-packages" &&
     echo "${NPMRC_PATCH}" > "${HOME}/.npmrc" &&
       true
   fi &&
+  npm install -g "${@}" &&
   mkdir --parents "${HOME}/bin" &&
   ln --symbolic --force "${HOME}/.npm-packages/bin/*" "${HOME}/bin"
   true
