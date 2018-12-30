da33523b89061c581557fb01a2c648b7ded89d3a
Emory Merryman <emory.merryman@gmail.com>
2018-12-30 14:56:12 +0000

    no telemetry, no welcome in atom

--- custom/other/atom/src/scripts/atom.sh
+++ custom/other/atom/src/scripts/atom.sh
@@ -3,7 +3,15 @@
 if [ ! -d "${HOME}/.atom" ]
 then
   mkdir "${HOME}/.atom" &&
-  ln --symbolic ${STORE_DIR}/atom/packages ${HOME}/.atom &&
+  (cat > ~/.atom/config.cson <<EOF
+"*":
+  core:
+    telemetryConsent: "no"
+  welcome:
+    showOnStartup: false
+EOF
+  ) &&
+  cp --recursive ${STORE_DIR}/atom/packages ${HOME}/.atom &&
   true
 fi &&
     atom "${@}" &&
