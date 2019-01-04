a61899f3aafe2ac6cb58cc7c25bebcd12791ac30
Emory Merryman <emory.merryman@gmail.com>
2019-01-04 22:41:56 +0000

    aws-cli-init

--- custom/scripts/aws-cli-init/aws-cli-init.sh
+++ custom/scripts/aws-cli-init/aws-cli-init.sh
@@ -1,7 +1,72 @@
 #!/bin/sh
 
-if [ ! -d ]
+while [ "${#}" -gt 0 ]
+do
+  case "${1}" in
+    --aws-access-key-id)
+      AWS_ACCESS_KEY_ID="${2}" &&
+        shift 2 &&
+        true
+    ;;
+    --aws-secret-access-key)
+      AWS_SECRET_ACCESS_KEY="${2}" &&
+        shift 2 &&
+        true
+    ;;
+    --region)
+      REGION="${2}" &&
+        shift 2 &&
+        true
+    ;;
+    --OUTPUT)
+      OUTPUT="${2}" &&
+        shift 2 &&
+        true
+    ;;
+    *)
+      echo "Unknown Option" &&
+        echo "${1}" &&
+        echo "${@}" &&
+        echo "${0}" &&
+        exit 66 &&
+        true
+    ;;
+  esac &&
+    true
+done &&
+if [ ! -d "${HOME}/.aws" ]
 then
-  aws init &&
+  mkdir "${HOME}/.aws" &&
+    true
 fi &&
+if [ ! -f "${HOME}/.aws/credentials" ]
+then
+  touch "${HOME}/.aws/credentials" &&
+    if [ ! -z "${AWS_ACCESS_KEY_ID}" ]
+    then
+      echo "aws_access_key_id=${AWS_ACCESS_KEY_ID}" >> "${HOME}/.aws/credentials" &&
+        true
+    fi &&
+    if [ ! -z "${AWS_SECRET_ACCESS_KEY}" ]
+    then
+      echo "aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}" >> "${HOME}/.aws/credentials" &&
+        true
+    fi &&
+        true
+fi &&
+if [ -f "${HOME}/.aws/config" ]
+then
+  touch "${HOME}/.aws/config" &&
+  if [ ! -z "${REGION}" ]
+  then
+    echo "region=${REGION}" >> "${HOME}/.aws/config" &&
+      true
+    fi &&
+    if [ ! -z "${OUTPUT}" ]
+    then
+      echo "output=${OUTPUT}" >> "${HOME}/.aws/config" &&
+        true
+      fi &&
+    true
+  fi &&
   true
