9560f9adc6d839d11fdea80e657cd7c6828371db
Emory Merryman <emory.merryman@gmail.com>
2019-01-04 18:03:22 +0000

    try a different url

--- custom/native/flash/default.nix
+++ custom/native/flash/default.nix
@@ -2,7 +2,7 @@
 pkgs.stdenv.mkDerivation {
   name = "flash";
   src = pkgs.fetchurl {
-    url = "https://github.com/nextmoose/nixos-configuration/blob/level-4/custom/expressions/chromium/src/flash_player_ppapi_linux.x86_64.tar.gz?raw=true";
+    url = "https://fpdownload.adobe.com/pub/flashplayer/pdc/32.0.0.101/flash_player_ppapi_linux.x86_64.tar.gz";
     sha512 = "3ya8l87wxclv5ybchhxypzl8dxpd3jm7bnnpzmw6jplfqhbx24bvcl4vsy7yxzj55b1g63nw5w7vxi608k7ri4ql0madgbsrrc8zqb4";
   };
   installPhase = ''
