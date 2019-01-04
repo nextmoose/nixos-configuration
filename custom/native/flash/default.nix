9934a064868449a7edd26b6c7ae30738ac2b12a0
Emory Merryman <emory.merryman@gmail.com>
2019-01-04 17:47:04 +0000

    different hash512

--- custom/native/flash/default.nix
+++ custom/native/flash/default.nix
@@ -3,7 +3,7 @@ pkgs.stdenv.mkDerivation {
   name = "flash";
   src = pkgs.fetchurl {
     url = "https://github.com/nextmoose/nixos-configuration/blob/level-4/custom/expressions/chromium/src/flash_player_ppapi_linux.x86_64.tar.gz?raw=true";
-    sha512 = "0zjyvhz9gal2ac1lqd4g51zwfhf5vfm84363jg3k3lk90pym97a68kf7syagj9aiv6lib78v9zx3nhnnmkimkpdv8jx3j919k2npj9r";
+    sha512 = "3ya8l87wxclv5ybchhxypzl8dxpd3jm7bnnpzmw6jplfqhbx24bvcl4vsy7yxzj55b1g63nw5w7vxi608k7ri4ql0madgbsrrc8zqb4";
   };
   installPhase = ''
     mkdir $out &&
