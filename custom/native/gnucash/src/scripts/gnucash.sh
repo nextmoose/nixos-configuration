076d0a99857b4992edce9b0d7373ad0da66a17cd
Emory Merryman <emory.merryman@gmail.com>
2019-01-04 21:22:40 +0000

    fix it so can read write .gconf.path

--- custom/native/gnucash/src/scripts/gnucash.sh
+++ custom/native/gnucash/src/scripts/gnucash.sh
@@ -3,7 +3,8 @@
 gnupg-import &&
 if [ ! -f "${HOME}/.gconf.path" ]
 then
-  cp ${STORE_DIR}/lib/gconf.path ${HOME}/.gconf.path &&
+  cat ${STORE_DIR}/lib/gconf.path > ${HOME}/.gconf.path &&
+  chmod 0600 "${HOME}/.gconf.path" &&
   gconftool-2 --shutdown &&
   true
 fi &&
