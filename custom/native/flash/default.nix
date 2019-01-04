e1dbaf7444ea202b595f675dce1c24859d5bc0c0
Emory Merryman <emory.merryman@gmail.com>
2019-01-04 17:17:01 +0000

    fix url

--- custom/native/flash/default.nix
+++ custom/native/flash/default.nix
@@ -3,7 +3,7 @@ pkgs.stdenv.mkDerivation {
   name = "flash";
   src = pkgs.fetchurl {
     name = "flash_player_ppapi_linux.x86_64.tar.gz";
-    url = "https://get.adobe.com/flashplayer/download/?installer=FP_32.0_for_Linux_64-bit_(.tar.gz)-_PPAPI&sType=7436&standalone=1";
+    url = "https://fpdownload.adobe.com/pub/flashplayer/pdc/32.0.0.101/flash_player_ppapi_linux.x86_64.tar.gz";
     sha512 = "231z9nvbmvd2hs83gnl6xbikyhm5ra1jka31394vdchwkahbwmp15j6kwxpb6y283swlyaf28bkdzycvlc2rdcsm07wp3izgk0k1z04";
   };
   installPhase = ''
