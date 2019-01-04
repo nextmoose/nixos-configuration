2acc1250535bc7e736ec02c7c63d368185d95077
Emory Merryman <emory.merryman@gmail.com>
2019-01-04 17:11:58 +0000

    change 512

--- custom/native/flash/default.nix
+++ custom/native/flash/default.nix
@@ -4,7 +4,7 @@ pkgs.stdenv.mkDerivation {
   src = pkgs.fetchurl {
     name = "flash_player_ppapi_linux.x86_64.tar.gz";
     url = "https://get.adobe.com/flashplayer/download/?installer=FP_32.0_for_Linux_64-bit_(.tar.gz)-_PPAPI&sType=7436&standalone=1";
-    sha512 = "9B71D224BD62F3785D96D46AD3EA3D73319BFBC2890CAADAE2DFF72519673CA72323C3D99BA5C11D7C7ACC6E14B8C5DA0C4663475C2E5C3ADEF46F73BCDEC043";
+    sha512 = "231z9nvbmvd2hs83gnl6xbikyhm5ra1jka31394vdchwkahbwmp15j6kwxpb6y283swlyaf28bkdzycvlc2rdcsm07wp3izgk0k1z04";
   };
   installPhase = ''
     mkdir $out &&
