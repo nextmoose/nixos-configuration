4dac9cec6081157b7639e876b0cfb5acf581d052
Emory Merryman <emory.merryman@gmail.com>
2019-01-04 17:33:47 +0000

    do not user name

--- custom/native/flash/default.nix
+++ custom/native/flash/default.nix
@@ -2,7 +2,6 @@
 pkgs.stdenv.mkDerivation {
   name = "flash";
   src = pkgs.fetchurl {
-    name = "flash_player_ppapi_linux.x86_64.tar.gz";
     url = "https://fpdownload.adobe.com/pub/flashplayer/pdc/32.0.0.101/flash_player_ppapi_linux.x86_64.tar.gz";
     sha512 = "0zjyvhz9gal2ac1lqd4g51zwfhf5vfm84363jg3k3lk90pym97a68kf7syagj9aiv6lib78v9zx3nhnnmkimkpdv8jx3j919k2npj9r";
   };
