f781c4a99d5402ddc3dc48404e6cf0d81c84387f
Emory Merryman <emory.merryman@gmail.com>
2019-01-24 21:29:19 +0000

    wtf

--- custom/containers/homebank.nix
+++ custom/containers/homebank.nix
@@ -64,7 +64,7 @@ in
   };
   config = { config, pkgs, ...}:
   {
-    filesystems = {
+    fileSystems = {
       "/mnt" = {
         "/device" = "/home/user/xxx.iso";
         "fsType" = "iso9660";
