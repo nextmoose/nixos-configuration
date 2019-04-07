a055535bfc32c3d631b0dee9f0492beaa4720e1f
Emory Merryman <emory.merryman@gmail.com>
2019-04-07 12:53:15 +0000

    do it

--- custom/staples.nix
+++ custom/staples.nix
@@ -2,35 +2,15 @@
   pkgs ? import <nixpkgs> {}
 } :
 rec {
-  cleanup-old-installation = (import ./assembly.nix {
+  create-installation-is = (import ./assembly.nix {
     pkgs = pkgs;
     name = "cleanup-old-installation";
-    src = ./assemblies/cleanup-old-installation;
+    src = ./assemblies/create-installation-iso;
     dependencies = [
       pkgs.utillinux
       pkgs.cryptsetup
       pkgs.lvm2
       pkgs.coreutils
-    ];
-  });
-  decrypt-installation-secrets = (import ./assembly.nix {
-    pkgs = pkgs;
-    name = "decrypt-installation-secrets";
-    src = ./assemblies/decrypt-installation-secrets;
-    dependencies = [
-      pkgs.mktemp
-      pkgs.coreutils
-      pkgs.gnupg
-      pkgs.gzip
-      pkgs.gnutar
-    ];
-  });
-  wpa-wifi = (import ./assembly.nix {
-    pkgs = pkgs;
-    name = "wpa-wifi";
-    src = ./assemblies/wpa-wifi;
-    dependencies = [
-      pkgs.wpa_supplicant
     ];
   });
 }
