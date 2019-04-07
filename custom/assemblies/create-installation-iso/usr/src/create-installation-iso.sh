e5453da91c91e2ba6048cc40bfc170b5aababea5
Emory Merryman <emory.merryman@gmail.com>
2019-04-07 12:58:42 +0000

    do it

--- custom/assemblies/create-installation-iso/usr/src/create-installation-iso.sh
+++ custom/assemblies/create-installation-iso/usr/src/create-installation-iso.sh
@@ -7,6 +7,6 @@ TEMP_DIR=$(mktemp -d) &&
   } &&
   trap cleanup EXIT &&
   read -s -p "SYMMETRIC PASSPHRASE? " SYMMETRIC_PASSPHRASE &&
-  cp "${STORE_DIR}/etc/nixos-live" "${TEMP_DIR}/iso.nix" &&
-  nix-build '<nixpkgs/nixos>' -A config.system.${DESTDIR}.isoImage -I nixos-config=iso.nix
+  cp "${STORE_DIR}/etc/nixos-live/iso.nix" "${TEMP_DIR}" &&
+  nix-build '<nixpkgs/nixos>' -A "config.system.${TEMP_DIR}.isoImage" -I nixos-config=iso.nix
   true
