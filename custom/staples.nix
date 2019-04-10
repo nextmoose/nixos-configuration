d038d42bb1ba8608b3ca43740d9eb08ed4609f72
Emory Merryman <emory.merryman@gmail.com>
2019-04-10 07:17:34 -0400

    configure nixos only changes files

--- custom/staples.nix
+++ custom/staples.nix
@@ -2,6 +2,21 @@
   pkgs ? import <nixpkgs> {}
 } :
 rec {
+  configure-nixos = (import ./create-derivation.nix {
+    pkgs = pkgs;
+    name = "configure-nixos";
+    src = ./derivations/install-local-nixos;
+    dependencies = [
+      pkgs.mktemp
+      pkgs.coreutils
+      pkgs.git
+      pkgs.rsync
+      pass
+      pkgs.gnutar
+      pkgs.mkpasswd
+      pkgs.gnused
+    ];
+  });
   install-local-nixos = (import ./create-derivation.nix {
     pkgs = pkgs;
     name = "install-local-nixos";
