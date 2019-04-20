f09db3c9f13dd05c96ee7c2b0a805eebf0460b2c
Emory Merryman <emory.merryman@gmail.com>
2019-04-20 11:34:16 -0400

    do not need pass dependency in rescue iso

--- custom/rescue/src/install-nixos.sh
+++ custom/rescue/src/install-nixos.sh
@@ -13,6 +13,26 @@ do
 		shift 2 &&
 		true
 	    ;;
+	--private-gpg-file)
+	    PRIVATE_GPG_FILE="${2}" &&
+		shift 2 &&
+		true
+	    ;;
+	--private-gpg2-file)
+	    PRIVATE_GPG2_FILE="${2}" &&
+		shift 2 &&
+		true
+	    ;;
+	--ownertrust-gpg-file)
+	    OWNERTRUST_GPG_FILE="${2}" &&
+		shift 2 &&
+		true
+	    ;;
+	--ownertrust-gpg2-file)
+	    OWNERTRUST_GPG2_FILE="${2}" &&
+		shift 2 &&
+		true
+	    ;;
 	*)
 	    echo Unsupported Option &&
 		echo "${1}" &&
@@ -42,16 +62,60 @@ done &&
 		export PATH=/run/wrappers/bin:/run/current-system/sw/bin:$(dirname $(which systemctl)):$(dirname $(which grep)) &&
 		sudo nixos-rebuild switch &&
 		true
+	elif [ -z "${PRIVATE_GPG_FILE}" ]
+	then
+	    echo Unspecified PRIVATE_GPG_FILE &&
+		exit 64 &&
+		true
+	elif [ ! -f "${PRIVATE_GPG_FILE}" ]
+	then
+	    echo "PRIVATE_GPG_FILE=${PRIVATE_GPG_FILE} does not exist" &&
+		exit 64 &&
+		true
+	elif [ -z "${PRIVATE_GPG2_FILE}" ]
+	then
+	    echo Unspecified PRIVATE_GPG2_FILE &&
+		exit 64 &&
+		true
+	elif [ ! -f "${PRIVATE_GPG2_FILE}" ]
+	then
+	    echo "PRIVATE_GPG2_FILE=${PRIVATE_GPG2_FILE} does not exist" &&
+		exit 64 &&
+		true
+	elif [ -z "${OWNERTRUST_GPG_FILE}" ]
+	then
+	    echo Unspecified OWNERTRUST_GPG_FILE &&
+		exit 64 &&
+		true
+	elif [ ! -f "${OWNERTRUST_GPG_FILE}" ]
+	then
+	    echo "OWNERTRUST_GPG_FILE=${OWNERTRUST_GPG_FILE} does not exist" &&
+		exit 64 &&
+		true
+	elif [ -z "${OWNERTRUST_GPG2_FILE}" ]
+	then
+	    echo Unspecified OWNERTRUST_GPG2_FILE &&
+		exit 64 &&
+		true
+	elif [ ! -f "${OWNERTRUST_GPG2_FILE}" ]
+	then
+	    echo "OWNERTRUST_GPG2_FILE=${OWNERTRUST_GPG2_FILE} does not exist" &&
+		exit 64 &&
+		true
 	fi &&
 	    rm --recursive --force "${TEMP_DIR}" &&
 	    true
     } &&
     trap cleanup EXIT &&
     sh "${STORE_DIR}/src/configure-nixos.sh" \
-	--canonical-remote "${CANONICAL_REMOTE}" \
-	--branch "${BRANCH}" \
-	--work-dir "${TEMP_DIR}" \
-	--config-dir /etc/nixos &&
+       --canonical-remote "${CANONICAL_REMOTE}" \
+       --branch "${BRANCH}" \
+       --work-dir "${TEMP_DIR}" \
+       --config-dir /etc/nixos \
+       --private-gpg-file "${PRIVATE_GPG_FILE}" \
+       --private-gpg2-file "${PRIVATE_GPG2_FILE}" \
+       --ownertrust-gpg-file "${OWNERTRUST_GPG_FILE}" \
+       --ownertrust-gpg2-file "${OWNERTRUST_GPG2_FILE}" &&
     export PATH=/run/wrappers/bin:/run/current-system/sw/bin:$(dirname $(which systemctl)):$(dirname $(which grep)) &&
     sudo nixos-rebuild switch &&
     true
