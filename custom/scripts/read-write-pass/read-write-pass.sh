7cc3ff22d23bd14fc8dac40f681e3ca1cb87625a
Emory Merryman <emory.merryman@gmail.com>
2019-04-20 18:31:32 -0400

    add read-write-pass

---
+++ custom/scripts/read-write-pass/read-write-pass.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+while [ "${#}" -gt 0 ]
+do
+    case "${1}" in
+	--gnupghome)
+	    export GNUPGHOME="${2}" &&
+		shift 2 &&
+		true
+	    ;;
+	--password-store-dir)
+	    export PASSWORD_STORE_DIR="${2}" &&
+		shift 2 &&
+		true
+	    ;;
+	--)
+	    ARGUMENTS="${@}" &&
+		shift &&
+		true
+	    ;;
+	*)
+	    echo Unsupported Option &&
+		echo "${1}" &&
+		echo "${0}" &&
+		echo "${@}" &&
+		exit 64 &&
+		true
+	    ;;
+    esac &&
+	true
+done &&
+    pass "${ARGUMENTS}" &&
+    true
