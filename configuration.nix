42ffd5fdc6b30c577c0b7c836c8ec45861057559
Emory Merryman <emory.merryman@gmail.com>
2019-04-21 10:45:56 -0400

    try foo service

--- configuration.nix
+++ configuration.nix
@@ -89,7 +89,7 @@ in
     stateVersion = "18.03";
   };
   systemd.services = {
-    foo = (import ./custom/docker-image {
+    foo = (import ./custom/docker-image.nix {
       name = "foo";
       entrypoint = "${pkgs.coreutils}/bin/echo";
       cmd = [ "world" ];
