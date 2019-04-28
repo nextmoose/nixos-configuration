4f1da4e016eb6fdf4a81533f2d68bf83ca09546b
Emory Merryman <emory.merryman@gmail.com>
2019-04-27 20:33:33 -0400

    do it

--- custom/docker-images.nix
+++ custom/docker-images.nix
@@ -6,6 +6,10 @@
   system-secrets-read-only-pass-image = (import ./docker-image.nix {
     pkgs = pkgs;
     name = "system-secrets-read-only-pass";
+    contents = [
+      ${staples.init-read-only-pass}
+      ${pkgs.pass}
+    ];
     entrypoint = ''
       ${staples.init-read-only-pass}/bin/init-read-only-pass \
         --remote https://github.com/nextmoose/secrets.git \
