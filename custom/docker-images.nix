54a6df3584bba297ba15858ac96ac0ac34490fa2
Emory Merryman <emory.merryman@gmail.com>
2019-04-27 20:22:34 -0400

    do it

--- custom/docker-images.nix
+++ custom/docker-images.nix
@@ -3,4 +3,14 @@
   staples
 }:
 {
+  system-secrets-read-only-pass-image = (import ./docker-image.nix {
+    pkgs = pkgs;
+    name = "system-secrets-read-only-pass";
+    entrypoint = ''
+      ${staples.init-read-only-pass}/bin/init-read-only-pass \
+        --remote https://github.com/nextmoose/secrets.git \
+	--branch master &&
+	${pkgs.pass}/bin/pass
+    '';
+  });
 }
