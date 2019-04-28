f4c4519c630c640297f73bee01fff01fb940c2b2
Emory Merryman <emory.merryman@gmail.com>
2019-04-28 08:37:54 -0400

    do it

--- custom/scripts/system-secrets-read-only-pass/system-secrets-read-only-pass.sh
+++ custom/scripts/system-secrets-read-only-pass/system-secrets-read-only-pass.sh
@@ -6,7 +6,7 @@ docker \
     --interactive \
     --tty \
     --rm \
-    --mount type=volume,source=$(docker container inspect --format '{{ range .Mounts }}{{ if eq .Destination "/home" }}{{ .Name }}{{ end }}{{ end }}' $(docker container ls --quiet init-system-secrets-read-only-pass)),target=/home,readonly=false \
+    --mount type=volume,source=$(docker container inspect --format '{{ range .Mounts }}{{ if eq .Destination "/home" }}{{ .Name }}{{ end }}{{ end }}' init-system-secrets-read-only-pass),target=/home,readonly=false \
     pass \
     "${@}" &&
     true
