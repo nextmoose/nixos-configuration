1d1d318c9206d9e68f8457f1653a180c67bed604
Emory Merryman <emory.merryman@gmail.com>
2019-04-27 20:38:24 -0400

    do it

--- custom/docker-images.nix
+++ custom/docker-images.nix
@@ -7,8 +7,8 @@
     pkgs = pkgs;
     name = "system-secrets-read-only-pass";
     contents = [
-      ${staples.init-read-only-pass}
-      ${pkgs.pass}
+      staples.init-read-only-pass
+      pkgs.pass
     ];
     entrypoint = ''
       ${staples.init-read-only-pass}/bin/init-read-only-pass \
