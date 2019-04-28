455c2768ce37205881fd9475785968480c60d0cd
Emory Merryman <emory.merryman@gmail.com>
2019-04-28 08:51:08 -0400

    do it

--- custom/scripts/system-secrets-read-only-pass/system-secrets-read-only-pass.sh
+++ custom/scripts/system-secrets-read-only-pass/system-secrets-read-only-pass.sh
@@ -6,7 +6,7 @@ docker \
     --interactive \
     --tty \
     --rm \
-    --env GIT_SSL_CAINFO=/etc/ssl/certs/ca-bundle.crt &&
+    --env GIT_SSL_CAINFO=/etc/ssl/certs/ca-bundle.crt \
     --mount type=volume,source=$(docker container inspect --format '{{ range .Mounts }}{{ if eq .Destination "/home" }}{{ .Name }}{{ end }}{{ end }}' init-system-secrets-read-only-pass),target=/home,readonly=false \
     pass \
     "${@}" &&
