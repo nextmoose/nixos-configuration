89170c9c097376c7d7566f8e36f45648b2c4affa
Emory Merryman <emory.merryman@gmail.com>
2019-05-05 11:54:05 -0400

    try user-setup

--- custom/user-setup/src/user-setup.sh
+++ custom/user-setup/src/user-setup.sh
@@ -19,13 +19,13 @@ WORK_DIR=$(mktemp -d) &&
     docker \
 	container \
 	create \
-	--cidfile "${WORK_DIR}/containers/system-secrets-read-only-pass" \
+	--cidfile "${WORK_DIR}/containers/system-secrets-read-only-pass.cid" \
 	--restart always \
 	--label uuid="${SYSTEM_SECRETS_READ_ONLY_PASS_CONTAINER_UUID}" \
 	$(docker-image-id "${READ_ONLY_PASS_IMAGE_UUID}") \
 	--remote https://github.com/nextmoose/secrets.git \
 	--branch master &&
-    find "${WORK_DIR}/containers" | while read FILE
+    find "${WORK_DIR}/containers" -name *.cid | while read FILE
     do
 	docker container start $(cat "${FILE}") &&
 	    true
