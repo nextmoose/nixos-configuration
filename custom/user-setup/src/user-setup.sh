443766b6d2a598f96f9f481777a5591b5bbbea9d
Emory Merryman <emory.merryman@gmail.com>
2019-05-05 11:17:42 -0400

    try user-setup

--- custom/user-setup/src/user-setup.sh
+++ custom/user-setup/src/user-setup.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-echo ${READ_ONLY_IMAGE_UUID} &&
-    ROIU=$(docker-image-id ${READ_ONLY_IMAGE_UUID}) &&
-    echo ${ROIU} &&
-    if [ -z "$(docker-image-id ${READ_ONLY_IMAGE_UUID})" ]
-    then
-	docker image load --input "${STORE_DIR}/images/read-only-pass.tar" --quiet &&
-	    true
-    fi &&
+if [ -z "$(docker-image-id ${READ_ONLY_PASS_IMAGE_UUID})" ]
+then
+    docker image load --input "${STORE_DIR}/images/read-only-pass.tar" --quiet &&
+	true
+fi &&
     true
