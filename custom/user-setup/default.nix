36cf6790ea023250081e7bb076e2960cf732d7d9
Emory Merryman <emory.merryman@gmail.com>
2019-05-05 11:45:18 -0400

    try user-setup

--- custom/user-setup/default.nix
+++ custom/user-setup/default.nix
@@ -12,6 +12,12 @@ let
     ];
     uuid = "5c6872cb-5274-4292-8894-514afe182845";
   });
+  dependencies = [
+    pkgs.docker
+    staples.docker-image-id
+    pkgs.mktemp
+    pkgs.findutils
+  ];
 in
 pkgs.stdenv.mkDerivation {
   name = "user-setup";
@@ -29,7 +35,7 @@ pkgs.stdenv.mkDerivation {
       makeWrapper \
         "$out/src/user-setup.sh" \
 	"$out/bin/user-setup" \
-	--set PATH "${pkgs.lib.makeBinPath [pkgs.docker staples.docker-image-id]}" \
+	--set PATH "${pkgs.lib.makeBinPath dependencies }" \
 	--set READ_ONLY_PASS_IMAGE_UUID "5c6872cb-5274-4292-8894-514afe182845" \
 	--set SYSTEM_SECRETS_READ_ONLY_PASS_IMAGE_UUID "99cc243e-2d33-4c3a-9872-e979d9b8809e" \
 	--set SYSTEM_SECRETS_READ_WRITE_PASS_IMAGE_UUID "2464f0e3-d01c-4259-a9b2-1f8cfd3c5d3a" \
