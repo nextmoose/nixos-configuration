97a96222400a683d5f620514c19614f9a8d36ffa
Emory Merryman <emory.merryman@gmail.com>
2019-05-06 22:09:08 -0400

    try again

--- custom/user/default.nix
+++ custom/user/default.nix
@@ -8,7 +8,7 @@
 } :
 let
   read-only-pass-image-uuid = "5c6872cb-5274-4292-8894-514afe182845";
-//  read-write-pass-image-uuid = "e553b626-3ef8-470c-8d00-5dd7e1da77a9";
+  read-write-pass-image-uuid = "e553b626-3ef8-470c-8d00-5dd7e1da77a9";
   system-secrets-read-only-container-uuid = "2a9f1b25-4c9e-4a4d-99a9-e31cdbcfe1b4";
   system-secrets-read-write-container-uuid = "7419053a-804d-4ee3-b754-c4e4bdd50ca9";
   read-only-pass-image = (import ./build-image.nix {
--- custom/user/src/user-setup.sh
+++ custom/user/src/user-setup.sh
@@ -16,28 +16,40 @@ WORK_DIR=$(mktemp -d) &&
 	    true
     fi &&
     mkdir "${WORK_DIR}/containers" &&
-    docker \
-	container \
-	create \
-	--cidfile "${WORK_DIR}/containers/system-secrets-read-only-pass.cid" \
-	--restart always \
-	--label uuid="${SYSTEM_SECRETS_READ_ONLY_PASS_CONTAINER_UUID}" \
-	$(docker-image-id "${READ_ONLY_PASS_IMAGE_UUID}") \
-	--remote https://github.com/nextmoose/secrets.git \
-	--branch master &&
-    docker \
-	container \
-	create \
-	--cidfile "${WORK_DIR}/containers/system-secrets-read-write-pass.cid" \
-	--restart always \
-	--label uuid="${SYSTEM_SECRETS_READ_WRITE_PASS_CONTAINER_UUID}" \
-	$(docker-image-id "${READ_WRITE_PASS_IMAGE_UUID}") \
-	--remote origin/nextmoose/secrets.git \
-	--branch master \
-	--host github.com \
-	--user git \
-	--committer-name "Emory Merryman" \
-	--committer-email "emory.merryman@gmail.com" &&
+    if [ -z "$(docker-container-id ${SYSTEM_SECRETS_READ_ONLY_PASS_CONTAINER_UUID})" ]
+    then
+        docker \
+		container \
+		create \
+		--cidfile "${WORK_DIR}/containers/system-secrets-read-only-pass.cid" \
+		--restart always \
+		--label uuid="${SYSTEM_SECRETS_READ_ONLY_PASS_CONTAINER_UUID}" \
+		$(docker-image-id "${READ_ONLY_PASS_IMAGE_UUID}") \
+		--remote https://github.com/nextmoose/secrets.git \
+		--branch master &&
+	    true
+    fi &&
+    if true
+    then
+	echo SKIPPING &&
+	    true
+    elif [ -z "$(docker-container-id ${SYSTEM_SECRETS_READ_WRITE_PASS_CONTAINER_UUID})" ]
+    then
+	docker \
+	    container \
+	    create \
+	    --cidfile "${WORK_DIR}/containers/system-secrets-read-write-pass.cid" \
+	    --restart always \
+	    --label uuid="${SYSTEM_SECRETS_READ_WRITE_PASS_CONTAINER_UUID}" \
+	    $(docker-image-id "${READ_WRITE_PASS_IMAGE_UUID}") \
+	    --remote origin/nextmoose/secrets.git \
+	    --branch master \
+	    --host github.com \
+	    --user git \
+	    --committer-name "Emory Merryman" \
+	    --committer-email "emory.merryman@gmail.com" &&
+	    true
+    fi &&
     find "${WORK_DIR}/containers" -name *.cid | while read FILE
     do
 	docker container start $(cat "${FILE}") &&
