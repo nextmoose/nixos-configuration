60b124cca1c07607fe04c94e87913754365e2e17
Emory Merryman <emory.merryman@gmail.com>
2019-05-08 21:18:32 -0400

    try using jq

--- custom/setup/src/setup.sh
+++ custom/setup/src/setup.sh
@@ -17,7 +17,7 @@ WORK_DIR=$(mktemp -d) &&
 	echo BBB &&
 	    CIDFILE="${WORK_DIR}/system-secrets-read-only-pass.cid" &&
 	    echo BBB 2 &&
-	    UUID=$(jq -r --from-file "${STORE_DIR}/src/queries/containers/system-secrets-read-only-pass.js") &&
+	    UUID=$(jq -r --from-file "${STORE_DIR}/src/queries/containers/system-secrets-read-only-pass.js" "${STORE_DIR}/uuids.json") &&
 	    echo BBB 3 &&
 	    IMAGE_ID=$(docker-image-id $(jq -r --from-file "${STORE_DIR}/src/queries/images/read-only-pass.js" "${STORE_DIR}/uuids.json")) &&
 	    echo BBB 4 &&
