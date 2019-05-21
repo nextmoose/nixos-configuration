2c7133cae68b358a3af6b723914caa2fea526c39
Emory Merryman <emory.merryman@gmail.com>
2019-05-21 18:10:46 -0400

    do it

--- custom/scripts/init-dot-ssh/init-dot-ssh.sh
+++ custom/scripts/init-dot-ssh/init-dot-ssh.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
-WORK_DIR=$(mktemp -d) &&
-    cleanup() {
-	rm --recursive --force "${WORK_DIR}" &&
-	    true
-    } &&
-    trap cleanup EXIT &&
-    mkdir "${HOME}/.ssh" &&
+mkdir "${HOME}/.ssh" &&
     chmod 0700 "${HOME}/.ssh" &&
     sed \
 	-e "s#\${HOME}#${HOME}#" \
