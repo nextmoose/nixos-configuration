a517c636bbc37a0934fad662c5c0ac04988ae1aa
Emory Merryman <emory.merryman@gmail.com>
2019-05-21 20:48:59 -0400

    try this

--- custom/scripts/init-dot-ssh/init-dot-ssh.sh
+++ custom/scripts/init-dot-ssh/init-dot-ssh.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-if [ ! -d "${HOME}/.ssh" ]
-   mkdir "${HOME}/.ssh" &&
-       chmod 0700 "${HOME}/.ssh" &&
-       sed \
-	   -e "s#\${HOME}#${HOME}#" \
-	   -e "w${HOME}/.ssh/config" \
-	   "${STORE_DIR}/src/config" &&
-       chmod 0400 "${HOME}/.ssh/config" &&
-       true
-fi &&
-       true
+mkdir "${HOME}/.ssh" &&
+    chmod 0700 "${HOME}/.ssh" &&
+    sed \
+	-e "s#\${HOME}#${HOME}#" \
+	-e "w${HOME}/.ssh/config" \
+	"${STORE_DIR}/src/config" &&
+    chmod 0400 "${HOME}/.ssh/config" &&
+    true
