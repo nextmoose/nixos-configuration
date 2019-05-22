d7db4cbd0e4ce0abc4c406cfed5287da110cab83
Emory Merryman <emory.merryman@gmail.com>
2019-05-22 12:36:14 -0400

    try password expiry with a small kludge

--- custom/scripts/pass-expiry/pass-expiry.sh
+++ custom/scripts/pass-expiry/pass-expiry.sh
@@ -3,7 +3,7 @@
 NOW=$(date +%s) &&
     pass git ls-tree -r master --name-only | grep ".gpg\$" | while read FILE
     do
-	LAST_COMMIT_DATE=$(git log -1 --format=%ct ${FILE}) &&
+	LAST_COMMIT_DATE=$(pass git log -1 --format=%ct ${FILE}) &&
 	    AGE=$((${NOW} - ${LAST_COMMIT_DATE})) &&
 	    PASSWORD_SIZE=$(($(pass show ${FILE%.gpg}) | wc -c)) &&
 	    echo -e "${FILE%.gpg}\t${AGE}\t${PASSWORD_SIZE}" &&
