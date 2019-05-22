f9c2df4ed618d8e125eb2a144397b3d570bde2f7
Emory Merryman <emory.merryman@gmail.com>
2019-05-22 08:54:51 -0400

    do it

--- custom/fabricated/persistent-container/default.nix
+++ custom/fabricated/persistent-container/default.nix
@@ -12,6 +12,15 @@ pkgs.stdenv.mkDerivation {
   installPhase = ''
     mkdir "$out" &&
       cp --recursive . "$out/src" &&
+      chmod 0500 "$out/src/entrypoint.sh" &&
+      cat "$out/src/at.txt" >> "$out/entrypoint.sh" &&
+      mkdir "$out/bin" &&
+      makeWrapper \
+        "$out/src/entrypoint.sh" \
+	"$out/bin/${name}" \
+	--set PATH "${pkgs.lib.makeBinPath [ pkgs.coreutils pkgs.bash ]}" \
+	--set STORE_DIR "$out" \
+	--set UUID "${uuid}" &&
       true
   '';
 }
