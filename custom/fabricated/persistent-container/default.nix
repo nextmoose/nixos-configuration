262d5f4cd142382799baac314dc134edf512a7dd
Emory Merryman <emory.merryman@gmail.com>
2019-05-22 09:15:55 -0400

    do it

--- custom/fabricated/persistent-container/default.nix
+++ custom/fabricated/persistent-container/default.nix
@@ -8,7 +8,7 @@
 pkgs.stdenv.mkDerivation {
   name = name;
   src = ./src;
-  buildInputs = [ pkgs.makeWrapper entrypoint ];
+  buildInputs = [ pkgs.makeWrapper ];
   installPhase = ''
     mkdir "$out" &&
       echo AAAAA 00100 &&
@@ -21,18 +21,18 @@ ${run}
 EOF
       ) &&
       echo AAAAA 00400 &&
-#      echo "${entrypoint} " > "$out/entrypoint.sh" &&
-#      echo AAAAA 00500 &&
-#      cat "$out/src/at.txt" >> "$out/entrypoint.sh" &&
-#      echo AAAAA 00600 &&
-#      mkdir "$out/bin" &&
-#      echo AAAAA 00700 &&
-#      makeWrapper \
-#        "$out/src/entrypoint.sh" \
-#	"$out/bin/${name}" \
-#	--set PATH "${pkgs.lib.makeBinPath [ pkgs.coreutils pkgs.bash ]}" \
-#	--set STORE_DIR "$out" \
-#	--set UUID "${uuid}" &&
+      echo "${entrypoint} " > "$out/entrypoint.sh" &&
+      echo AAAAA 00500 &&
+      cat "$out/src/at.txt" >> "$out/entrypoint.sh" &&
+      echo AAAAA 00600 &&
+      mkdir "$out/bin" &&
+      echo AAAAA 00700 &&
+      makeWrapper \
+        "$out/src/entrypoint.sh" \
+	"$out/bin/${name}" \
+	--set PATH "${pkgs.lib.makeBinPath [ pkgs.coreutils pkgs.bash ]}" \
+	--set STORE_DIR "$out" \
+	--set UUID "${uuid}" &&
       true
   '';
 }
