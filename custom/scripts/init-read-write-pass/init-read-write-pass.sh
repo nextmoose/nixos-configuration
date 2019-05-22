345ef48094dfe67d728bf076d8c896b048739acf
Emory Merryman <emory.merryman@gmail.com>
2019-05-22 15:35:26 -0400

    add phonetic

--- custom/scripts/init-read-write-pass/init-read-write-pass.sh
+++ custom/scripts/init-read-write-pass/init-read-write-pass.sh
@@ -106,6 +106,6 @@ done &&
     pass git config user.email "${COMMITTER_EMAIL}" &&
     ln --symbolic $(which post-commit) "${HOME}/.password-store/.git/hooks" &&
     mkdir "${HOME}/.password-store/.extensions" &&
+    ln --symbolic $(which phonetic) "${HOME}/.password-store/.extensions/phonetic.bash" &&
     ln --symbolic $(which pass-expiry) "${HOME}/.password-store/.extensions/pass-expiry.bash" &&
-    ln --symbolic $(which phonetic) "${HOME}/.password-store/.extentions/phonetic.bash" &&
     true
