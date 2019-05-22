d357c60c53e8826e9ed7e0fae3d122a6b98d76a6
Emory Merryman <emory.merryman@gmail.com>
2019-05-22 08:57:19 -0400

    do it

--- custom/fabricated/persistent-container/default.nix
+++ custom/fabricated/persistent-container/default.nix
@@ -11,10 +11,22 @@ pkgs.stdenv.mkDerivation {
   buildInputs = [ pkgs.makeWrapper entrypoint ];
   installPhase = ''
     mkdir "$out" &&
+      echo AAAAA 00100 &&
       cp --recursive . "$out/src" &&
+      echo AAAAA 00200 &&
       chmod 0500 "$out/src/entrypoint.sh" &&
+      echo AAAAA 00300 &&
+      (cat > "$out/run.sh" <<EOF
+${run}
+EOF
+      ) &&
+      echo AAAAA 00400 &&
+      echo "${entrypoint} " > "$out/entrypoint.sh" &&
+      echo AAAAA 00500 &&
       cat "$out/src/at.txt" >> "$out/entrypoint.sh" &&
+      echo AAAAA 00600 &&
       mkdir "$out/bin" &&
+      echo AAAAA 00700 &&
       makeWrapper \
         "$out/src/entrypoint.sh" \
 	"$out/bin/${name}" \
