6edf44d1883cbcf4df268d662836055d8706abb0
Emory Merryman <emory.merryman@gmail.com>
2019-05-23 11:29:07 -0400

    do it

--- custom/scripts/init-gnupg/init-gnupg.sh
+++ custom/scripts/init-gnupg/init-gnupg.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-jq -r . ${STORE_DIR}/configuration.json &&
+echo AAAAAAAAAAAAAAAA 00100 &&
     gpg --batch --import $(jq -r .gpg.key ${STORE_DIR}/configuration.json) &&
     gpg --import-ownertrust $(jq -r .gpg.ownertrust ${STORE_DIR}/configuration.json) &&
     gpg2 --import $(jq -r .gpg2.key ${STORE_DIR}/configuration.json) &&
