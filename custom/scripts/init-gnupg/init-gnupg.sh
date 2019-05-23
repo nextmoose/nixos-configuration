268b47fb8ae5425473b6841595ca1c3627db35c7
Emory Merryman <emory.merryman@gmail.com>
2019-05-22 20:23:11 -0400

    add a challenge script

--- custom/scripts/init-gnupg/init-gnupg.sh
+++ custom/scripts/init-gnupg/init-gnupg.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
 jq -r . ${STORE_DIR}/configuration.json &&
+    echo A 00100 &&
     gpg --batch --import $(jq -r .gpg.key ${STORE_DIR}/configuration.json) &&
+    echo A 00100 &&
     gpg --import-ownertrust $(jq -r .gpg.ownertrust ${STORE_DIR}/configuration.json) &&
+    echo A 00200 &&
     gpg2 --import $(jq -r .gpg2.key ${STORE_DIR}/configuration.json) &&
+    echo A 00300 &&
     gpg2 --import-ownertrust $(jq -r .gpg2.ownertrust ${STORE_DIR}/configuration.json) &&
+    echo A 00400 &&
     true
