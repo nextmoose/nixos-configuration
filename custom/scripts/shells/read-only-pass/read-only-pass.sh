e7f2cd9ddcad8725773c4b70124ba7c1c4bc186e
Emory Merryman <emory.merryman@gmail.com>
2019-05-23 11:25:04 -0400

    do it

--- custom/scripts/shells/read-only-pass/read-only-pass.sh
+++ custom/scripts/shells/read-only-pass/read-only-pass.sh
@@ -8,11 +8,11 @@ export HOME="$(mktemp -d)" &&
     } &&
     trap cleanup EXIT &&
     cd "${HOME}" &&
-    init-gnupg &&
-    pass init "$(gnupg-key-id)" &&
-    pass git init &&
-    pass git remote add canonical "${1}" &&
-    pass git fetch canonical "${2}" &&
-    pass git checkout "canonical/${2}" &&
-    ln --symbolic "$(which pre-commit)" "${HOME}/.password-store/.git/hooks" &&
+#    init-gnupg &&
+#    pass init "$(gnupg-key-id)" &&
+#    pass git init &&
+#    pass git remote add canonical "${1}" &&
+#    pass git fetch canonical "${2}" &&
+#    pass git checkout "canonical/${2}" &&
+#    ln --symbolic "$(which pre-commit)" "${HOME}/.password-store/.git/hooks" &&
     true
