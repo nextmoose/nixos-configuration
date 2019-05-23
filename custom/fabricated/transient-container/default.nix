1487fdc86646d59765fe011a62147212d1afa11f
Emory Merryman <emory.merryman@gmail.com>
2019-05-22 21:22:21 -0400

    add transient containers

--- custom/fabricated/transient-container/default.nix
+++ custom/fabricated/transient-container/default.nix
@@ -11,13 +11,13 @@ pkgs.stdenv.mkDerivation {
     mkdir "$out" &&
       cp --recursive . "$out/src" &&
       chmod 0500 "$out/src/entrypoint.sh" &&
-      sh "$out/src/write-entrypoint-script.sh" "${entrypoint}" > "$out/entrypoint.sh" &&
       mkdir "$out/bin" &&
       makeWrapper \
         "$out/src/entrypoint.sh" \
 	"$out/bin/${name}" \
 	--set PATH "${pkgs.lib.makeBinPath [ pkgs.coreutils pkgs.bash pkgs.mktemp ]}" \
-	--set STORE_DIR "$out" \
+	--set ENTRYPOINT "${entrypoint}" \
+	--set STORE_DIR "$out" &&
       true
   '';
 }
