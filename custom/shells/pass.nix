a91ab246b4cc25e819df6d76aa40a1b1869b57a6
Emory Merryman <emory.merryman@gmail.com>
2019-05-23 11:14:31 -0400

    do it

--- custom/shells/pass.nix
+++ custom/shells/pass.nix
@@ -2,10 +2,11 @@
   pkgs ? import <nixpkgs> {},
   shell-scripts ? import /etc/nixos/custom/shell-scripts.nix {
     pkgs = pkgs;
-    canonical-remote ? https://github.com/nextmoose/secrets.git;
+    canonical-remote ? "https://github.com/nextmoose/secrets.git";
+    branch ? "master";
   }
 }:
 pkgs.mkShell {
   buildInputs = [ shell-scripts ];
-  shellHook = "${shell-scripts.pass}/bin/pass ${canonical-remote}";
+  shellHook = "${shell-scripts.pass}/bin/pass ${canonical-remote} ${branch}";
 }
