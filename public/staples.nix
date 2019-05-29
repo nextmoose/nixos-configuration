5e0b941bb5ee66e297aae4fc5f58b7dc29d63a76
Emory Merryman <emory.merryman@gmail.com>
2019-05-29 19:30:30 -0400

    try this

--- public/staples.nix
+++ public/staples.nix
@@ -2,4 +2,9 @@
   pkgs
 } :
 rec {
+  configure-nixos = (import ./utils/script-derivation.nix {
+    pkgs = pkgs;
+    name = "configure-nixos";
+    src = scripts/configure-nixos;
+  });
 }
--- public/tests/specs/public/scripts/configure-nixos.nix
+++ public/tests/specs/public/scripts/configure-nixos.nix
@@ -1,12 +1,13 @@
 import /nix/store/wl7y85xg46dsl5a7jjvqqdg1zbf678zn-nixos-18.03.133389.b551f89e256/nixos/nixos/tests/make-test.nix {
-  machine = (import ../../../configuration.nix {
-    config = {};
-    pkgs = {
-    };
-  });
+  machine = { pkgs, ... } : {
+    environment.systemPackages = [
+      pkgs.emacs
+    ];
+  };
   testScript = ''
     $machine->start;
     $machine->waitForUnit('multi-user.target');
+    $machine->waitUntilSucceeds("ls /");
     $machine->screenshot('shot00100');
     $machine->shutdown;
   '';
