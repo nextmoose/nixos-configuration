61b40b32ae81c1ad85b040b27a6b4d37886ef83f
Emory Merryman <emory.merryman@gmail.com>
2019-05-30 08:22:34 -0400

    enableOCR=true now what

--- public/tests/specs/public/scripts/configure-nixos.nix
+++ public/tests/specs/public/scripts/configure-nixos.nix
@@ -18,20 +18,22 @@ import /nix/store/wl7y85xg46dsl5a7jjvqqdg1zbf678zn-nixos-18.03.133389.b551f89e25
       };
     };
   };
+  enableOCR = true;
   testScript = ''
     $machine->start;
     $machine->waitForUnit('multi-user.target');
     $machine->sendChars("user\n");
-    $machine->waitUntilSucceeds("sleep 10");
+    $machine->waitUntilSucceeds("sleep 1");
     $machine->sendChars("password\n");
-    $machine->waitUntilSucceeds("sleep 10");
+    $machine->waitUntilSucceeds("sleep 2");
     $machine->sendChars("mkdir /tmp/source\n");
     $machine->sendChars("mkdir /tmp/source/public\n");
     $machine->sendChars("touch /tmp/source/configuration.nix\n");
     $machine->sendChars("mkdir /tmp/work\n");
-    $machine->sendChars("configure-nixos --work-dir /tmp/wor --source-dir /tmp/source\n");
-    $machine->waitUntilSucceeds("sleep 10");
+    $machine->sendChars("configure-nixos --work-dir /tmp/work --source-dir /tmp/source\n");
+    $machine->waitUntilSucceeds("sleep 3");
     $machine->screenshot('shot00100');
+    $machine->getScreenText('whatever', 'wtf');
     $machine->shutdown;
   '';
 }
