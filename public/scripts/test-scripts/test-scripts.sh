821d9a891b7570d67fd3667a06f0cb4884981e89
Emory Merryman <emory.merryman@gmail.com>
2019-06-14 17:37:12 -0400

    rewrite

--- public/scripts/test-scripts/test-scripts.sh
+++ public/scripts/test-scripts/test-scripts.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
 
-chromium $(jq --raw-output ".[\"${@}\"]" "${STORE_DIR}/configuration.json")/log.html &&
+while [ "${#}" -gt 0 ]
+do
+    case "${1}" in
+	--results)
+	    chromium $(jq --raw-output ".[\"${@}\"]" "${STORE_DIR}/configuration.json")/log.html &&
+		shift &&
+		true
+	    ;;
+	*)
+	    echo Unsupported Option &&
+		echo "${1}" &&
+		echo "${@}" &&
+		echo "${0}" &&
+		exit 64 &&
+		true
+	    ;;
+    esac &&
+	true
+done &&
     true
