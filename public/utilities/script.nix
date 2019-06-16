a1595e55110cca828e4b543dbd9d64662c8e1765
Emory Merryman <emory.merryman@gmail.com>
2019-06-16 17:46:48 -0400

    do it

---
+++ public/utilities/script.nix
@@ -0,0 +1,37 @@
+{
+  pkgs
+}:
+{
+  name,
+  src,
+  dependencies ? [],
+  configuration ? {},
+  test-script
+}:
+rec {
+  implementation = pkgs.stdenv.mkDerivation {
+    name = name;
+    src = src;
+    buildInputs = [ pkgs.makeWrapper ];
+    installPhase = ''
+      mkdir $out &&
+        cp --recursive . "$out/src" &&
+        chmod 0500 "$out/src/${name}.sh" &&
+        mkdir "$out/bin" &&
+        makeWrapper \
+          "$out/src/${name}.sh" \
+	  "$out/bin/${name}" \
+	   --set PATH "${pkgs.lib.makeBinPath dependencies}" \
+	   --set STORE_DIR "$out" &&
+        echo '${builtins.toJSON configuration}' > "$out/configuration.json" &&
+        true
+     '';
+  };
+  testing = {
+    results = (import ./script-test.nix {
+      implementation = implementation;
+      test-script = test-script;
+    });
+    mutants = map (d: builtins.filter (x: x!=d) dependencies) dependencies;
+  };
+}
